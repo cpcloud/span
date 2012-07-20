11e88791b2916f86f3f45efe5aaf24e81b4b7f6a
Phillip Cloud <cpcloud@gmail.com>
2012-07-20 15:16:02 -0400

    Got rid of create_times function

--- clear_refrac/__init__.py
+++ clear_refrac/__init__.py
@@ -1,4 +1,3 @@
-from _clear_refrac import (clear_refrac as clearref, thresh, create_times,
-                           thresh_and_clear)
+from _clear_refrac import (clear_refrac as clearref, thresh,  thresh_and_clear)
 
-__all__ = 'clearref', 'thresh', 'create_times', 'thresh_and_clear'
+__all__ = 'clearref', 'thresh', 'thresh_and_clear'
