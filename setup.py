afcae438827014f51432ea63d9b52f1f1f944eaf
Phillip Cloud <cpcloud@gmail.com>
2012-09-10 10:52:26 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@
 
 import os
 import platform
+import glob
+import shutil
 
 import numpy as np
 from distutils.core import setup
@@ -15,13 +17,14 @@ if platform.system().lower() == 'linux':
     
 
 ext_modules = [Extension('_clear_refrac',
-                         [os.path.join('span', 'clear_refrac',
+                         [os.path.join('span', 'utils',
                                        'clear_refrac%spyx' % os.extsep)],
                          extra_compile_args=extra_compile_args,
                          include_dirs=[np.get_include()])]
 
 if __name__ == '__main__':
-    with open('README.txt') as f:
+    readme_filename = glob.glob('README*')[0]
+    with open(readme_filename, 'r') as f:
         readme = f.read()
     setup(name='span',
           version='0.1',
@@ -32,6 +35,8 @@ if __name__ == '__main__':
           license='LICENSE.txt',
           description='Spike train analysis',
           long_description=readme,
-          install_requires=[''],
+          install_requires=['Cython >= 0.17', 'numpy >= 1.6.0',
+                            'scipy >= 0.10.0', 'pandas >= 0.8.0',
+                            'clint >= 0.2.1', 'matplotlib >= 1.1.1'],
           ext_modules=ext_modules,
           cmdclass={'build_ext': build_ext})
