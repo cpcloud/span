26fcc22d6a15d7f0c23e02a749843f743adb7273
Phillip Cloud <cpcloud@gmail.com>
2012-09-10 10:16:59 -0400

    rm bunch

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 import os
+import platform
 
+import numpy as np
 from distutils.core import setup
+
+from Cython.Distutils import build_ext
+from Cython.Distutils.extension import Extension
+
+extra_compile_args = []
+if platform.system().lower() == 'linux':
+    extra_compile_args.append('-march=native')
+    
+
+ext_modules = [Extension('_clear_refrac',
+                         [os.path.join('span', 'clear_refrac',
+                                       'clear_refrac%spyx' % os.extsep)],
+                         extra_compile_args=extra_compile_args,
+                         include_dirs=[np.get_include()])]
 
 if __name__ == '__main__':
     with open('README.txt') as f:
@@ -14,4 +30,6 @@ if __name__ == '__main__':
           license='LICENSE.txt',
           description='Spike train analysis',
           long_description=readme,
-          install_requires=['', ''])
+          install_requires=[''],
+          ext_modules=ext_modules,
+          cmdclass={'build_ext': build_ext})
