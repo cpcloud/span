a076e385ee686f5c0075854c6811e9f6d4870743
Phillip Cloud <cpcloud@gmail.com>
2012-09-11 14:39:53 -0400

    add new functions for importing

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -1,6 +1,6 @@
 from .utils import (cast, ndtuples, dirsize, ndlinspace, nans, remove_legend,
                     name2num, group_indices, flatten, bin_data, summary,
                     nextpow2, fractional, zeropad, pad_larger, iscomplex,
-                    spike_window)
+                    spike_window, distance_map, get_fft_funcs, num2name)
 from ._clear_refrac import (clear_refrac, clear_refrac_out, thresh,
                             thresh_and_clear, thresh_out)
