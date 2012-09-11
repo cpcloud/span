c9dd52bd5f7dae1a5c34e852bd2ddc7776a9520d
Phillip Cloud <cpcloud@gmail.com>
2012-09-11 12:45:01 -0400

    final setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ import platform
 import glob
 import shutil
 
-import numpy as np
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('NumPy is not installed on your system')
+
+try:
+    import Cython
+except ImportError:
+    raise ImportError('Cython is not installed on your system')
+
 from distutils.core import setup
 
 from Cython.Distutils import build_ext
@@ -31,13 +40,13 @@ if __name__ == '__main__':
           version='0.1',
           author='Phillip Cloud',
           author_email='cpcloud@gmail.com',
-          packages=['span'],
+          packages=['span', 'span.tdt', 'span.utils'],
           scripts=[os.path.join('bin', 'serv2mat.py')],
+          ext_modules=ext_modules,
           license='LICENSE.txt',
           description='Spike train analysis',
           long_description=readme,
-          install_requires=['Cython >= 0.17', 'numpy >= 1.6.0',
-                            'scipy >= 0.10.0', 'pandas >= 0.8.0',
-                            'clint >= 0.2.1', 'matplotlib >= 1.1.1'],
-          ext_modules=ext_modules,
+          install_requires=['Cython >=0.17', 'numpy >=1.6.0',
+                            'scipy >=0.10.0', 'pandas >=0.8.0',
+                            'matplotlib >=1.1.1'],
           cmdclass={'build_ext': build_ext})
