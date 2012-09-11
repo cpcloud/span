58c7a77815b5c6a92ecb6e357242e38b472b19d0
Phillip Cloud <cpcloud@gmail.com>
2012-09-11 12:33:44 -0400

    ahh incorrct module

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if platform.system().lower() == 'linux':
     extra_compile_args.append('-march=native')
     
 
-utils_dir = os.path.join(os.path.getcwd(), 'span', 'utils')
+utils_dir = os.path.join(os.getcwd(), 'span', 'utils')
 ext_modules = [Extension('_clear_refrac',
                          [os.path.join(utils_dir,
                                        'clear_refrac%spyx' % os.extsep)],
