f158d07c7e0ecceb6c26ac2c7a817454989e7195
Phillip Cloud <cpcloud@gmail.com>
2012-09-16 18:33:14 -0400

    Add new utility functions to import

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -1,6 +1,8 @@
 from utils import (cast, ndtuples, dirsize, ndlinspace, nans, remove_legend,
                    name2num, group_indices, flatten, bin_data, summary,
                    nextpow2, fractional, zeropad, pad_larger2, pad_larger,
-                   iscomplex, distance_map, get_fft_funcs, num2name)
+                   iscomplex, distance_map, get_fft_funcs, num2name, isvector,
+                   detrend_none, detrend_mean, detrend_linear, hascomplex,
+                   electrode_distance, distance_map)
 from _clear_refrac import (clear_refrac, clear_refrac_out, thresh,
                            thresh_and_clear, thresh_out)
