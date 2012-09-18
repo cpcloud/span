17995aee8bdf652e836543f072ef41d86b8c6d6e
Phillip Cloud <cpcloud@gmail.com>
2012-09-18 18:40:50 -0400

    update imports

--- span/__init__.py
+++ span/__init__.py
@@ -1,3 +1,2 @@
 import span.tdt
 import span.utils
-from span.utils import Series, DataFrame
--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -4,7 +4,5 @@ from utils import (cast, ndtuples, dirsize, ndlinspace, nans, remove_legend,
                    iscomplex, distance_map, get_fft_funcs, num2name, isvector,
                    detrend_none, detrend_mean, detrend_linear, hascomplex,
                    electrode_distance, distance_map)
-from corrseries import Series
-from corrdataframe import DataFrame
 from _clear_refrac import (clear_refrac, clear_refrac_out, thresh,
                            thresh_and_clear, thresh_out)
