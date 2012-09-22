d065a569f1872b818e9f9aca0064d5433c66f155
Phillip Cloud <cpcloud@gmail.com>
2012-09-22 13:37:18 -0400

    add test skeletons

---
+++ span/utils/test/test_bin_data.py
@@ -0,0 +1,4 @@
+from _bin_data import bin_data
+
+def test_bin_data():
+    pass
---
+++ span/utils/test/test_clear_refrac.py
@@ -0,0 +1,22 @@
+from _clear_refrac import (
+    clear_refrac, clear_refrac_out, thresh, thresh_and_clear, thresh_out)
+
+
+def test_clear_refrac():
+    pass
+
+
+def test_clear_refrac_out():
+    pass
+
+
+def test_thresh():
+    pass
+
+
+def test_thresh_and_clear():
+    pass
+
+
+def test_thresh_out():
+    pass
