3b6fdf10e5c59b254419a000b0dcb243976c5f2d
Phillip Cloud <cpcloud@gmail.com>
2012-09-23 18:32:15 -0400

    add comppile args

--- setup.py
+++ setup.py
@@ -3,17 +3,11 @@
 import os
 import platform
 import glob
-import shutil
 
 try:
     import numpy as np
 except ImportError:
     raise ImportError('NumPy is not installed on your system')
-
-try:
-    import Cython
-except ImportError:
-    raise ImportError('Cython is not installed on your system')
 
 from distutils.core import setup
 
@@ -21,21 +15,28 @@ from Cython.Distutils import build_ext
 from Cython.Distutils.extension import Extension
 
 extra_compile_args = []
+extra_link_args = []
+
 if platform.system().lower() == 'linux':
     extra_compile_args.append('-march=native')
+    extra_compile_args.append('-O3')
 
+npy_includes = np.get_include()
+include_dirs = [npy_includes]
 
 utils_dir = os.path.join('span', 'utils')
 ext_modules = [Extension('_clear_refrac',
                          [os.path.join(utils_dir,
-                                       'clear_refrac%spyx' % os.extsep)],
+                                       'clear_refrac{sep}pyx'.format(sep=os.extsep))],
                          extra_compile_args=extra_compile_args,
-                         include_dirs=[np.get_include()]),
+                         extra_link_args=extra_link_args,
+                         include_dirs=include_dirs),
                Extension('_bin_data',
                          [os.path.join(utils_dir,
-                                       'bin_data%spyx' % os.extsep)],
+                                       'bin_data{sep}pyx'.format(sep=os.extsep))],
                          extra_compile_args=extra_compile_args,
-                         include_dirs=[np.get_include()])]
+                         extra_link_args=extra_link_args,
+                         include_dirs=include_dirs)]
 
 if __name__ == '__main__':
     readme_filename = glob.glob('README*')[0]
