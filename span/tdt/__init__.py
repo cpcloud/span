1775be9fccee9e7c2aa61e24b03d08758d7fbbbd
Phillip Cloud <cpcloud@gmail.com>
2012-09-26 20:10:19 -0400

    add new indexer to exported globals

--- span/tdt/__init__.py
+++ span/tdt/__init__.py
@@ -1,3 +1,3 @@
 from span.tdt.spikedataframe import SpikeDataFrame
-from span.tdt.spikeglobals import Indexer, ShankMap
+from span.tdt.spikeglobals import Indexer, ShankMap, SortedIndexer, ChannelIndex
 from span.tdt.tank import PandasTank
