0834857a7610bba3aabb6dbcffedcea86c4bb4dd
Phillip Cloud <cpcloud@gmail.com>
2012-09-29 23:25:16 -0400

    fix imports

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -2,9 +2,9 @@ from utils import (
     cast, detrend_linear, detrend_mean, detrend_none, dirsize, distance_map,
     electrode_distance, fractional, get_fft_funcs, group_indices, hascomplex,
     iscomplex, isvector, name2num, nans, ndlinspace, ndtuples, nextpow2,
-    num2name, pad_larger, pad_larger2, remove_legend, cartesian)
+    num2name, pad_larger, pad_larger2, remove_legend, cartesian, compose,
+    composemap)
 from decorate import thunkify, cached_property
-from functional import compose, composemap
-from server import AbstractServer, ArodServer
-from _clear_refrac import clear_refrac
-from _bin_data import bin_data
+from span.utils._clear_refrac import clear_refrac
+from span.utils._bin_data import bin_data
+from span.utils.trimmean import trimmean
