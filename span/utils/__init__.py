6cc4742497dea8dffb5a72b6bb8f71368634cdb3
Phillip Cloud <cpcloud@gmail.com>
2012-10-02 15:41:34 -0400

    update imports

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -1,9 +1,9 @@
 from utils import (
     cast, detrend_linear, detrend_mean, detrend_none, dirsize, distance_map,
-    electrode_distance, fractional, get_fft_funcs, group_indices, hascomplex,
-    iscomplex, isvector, name2num, nans, ndlinspace, ndtuples, nextpow2,
-    num2name, pad_larger, pad_larger2, remove_legend, cartesian, compose,
-    composemap, trimmean, nans_like)
-from decorate import thunkify, cached_property
+    fractional, get_fft_funcs, group_indices, hascomplex, iscomplex, isvector,
+    name2num, nans, ndlinspace, ndtuples, nextpow2, num2name, pad_larger,
+    pad_larger2, remove_legend, cartesian, compose, composemap, trimmean,
+    nans_like)
+from span.utils.decorate import thunkify, cached_property
 from span.utils._clear_refrac import clear_refrac
 from span.utils._bin_data import bin_data
