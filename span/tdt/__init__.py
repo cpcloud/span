b4fe1a7e3db791410db4f5b22a99806786263d0d
Phillip Cloud <cpcloud@gmail.com>
2012-10-04 11:21:17 -0400

    import distance_map to tdt toplevel

--- span/tdt/__init__.py
+++ span/tdt/__init__.py
@@ -1,4 +1,4 @@
 from span.tdt.spikedataframe import SpikeDataFrame
 from span.tdt.spikeglobals import Indexer, ShankMap, SortedIndexer, ChannelIndex
 from span.tdt.tank import PandasTank
-from span.tdt.recording import ElectrodeMap
+from span.tdt.recording import distance_map, ElectrodeMap
