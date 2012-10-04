9c551efb355880547c8486ca5165bd1561d577ca
Phillip Cloud <cpcloud@gmail.com>
2012-10-04 11:22:40 -0400

    add schedule for prange and get rid of unnecessary C-level function

--- span/utils/clear_refrac.pyx
+++ span/utils/clear_refrac.pyx
@@ -9,7 +9,7 @@ ctypedef np.uint8_t uint8
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef void _clear_refrac(np.ndarray[uint8, ndim=2, cast=True] a, long window):
+def clear_refrac(np.ndarray[uint8, ndim=2, cast=True] a not None, long window):
     cdef:
         long channel, i, sample
         long nsamples = a.shape[0], nchannels = a.shape[1]
@@ -17,16 +17,13 @@ cdef void _clear_refrac(np.ndarray[uint8, ndim=2, cast=True] a, long window):
 
     for channel in xrange(nchannels):
         sample = 0
+
         while sample + window < nsamples:
             if a_data[sample * nchannels + channel]:
                 with nogil, parallel():
-                    for i in prange(sample + 1, sample + window + 1):
+                    for i in prange(sample + 1, sample + window + 1,
+                                    schedule='guided'):
                         a_data[i * nchannels + channel] = 0
                 sample += window
             sample += 1
 
-
-@cython.wraparound(False)
-@cython.boundscheck(False)
-def clear_refrac(np.ndarray[uint8, ndim=2, cast=True] a not None, long window):
-    _clear_refrac(a, window)
