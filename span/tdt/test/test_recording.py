c3bcd81fdf372d29a2e982f5d053b189703e5b98
Phillip Cloud <cpcloud@gmail.com>
2012-10-04 11:22:04 -0400

    add test class for distance_map FUNCTION

--- span/tdt/test/test_recording.py
+++ span/tdt/test/test_recording.py
@@ -1,10 +1,30 @@
+from future_builtins import map, zip
+
 import unittest
+import itertools as it
 
 import numpy as np
 import pandas as pd
 
-def test_distance_map():
-    assert False
+from span.tdt import distance_map, ElectrodeMap
+
+
+class TestDistanceMap(unittest.TestCase):
+    
+    def test_distance_map(self):
+        nshanks = np.arange(9)
+        elecs = np.arange(11)
+        between = np.arange(start=0, stop=400, step=25)
+        within = between.copy()
+        metrics = 'wminkowski',
+        ps = np.arange(10) + 1
+        arg_sets = it.product(nshanks, elecs, between, within, metrics, ps)
+        for arg_set in arg_sets:
+            nsh, nelec, btwn, wthn, metr, p = arg_set
+            if not nsh or not nelec:
+                self.assertRaises(AssertionError, distance_map, nsh, nelec, btwn,
+                                  wthn, metr, p)
+            dm = distance_map(nsh, nelec, btwn, wthn, metr, p)
 
 
 class TestElectrodeMap(unittest.TestCase):
