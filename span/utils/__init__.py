dd0066393e931a9e8bf2691025f98814a0492541
Phillip Cloud <cpcloud@gmail.com>
2012-10-06 12:51:45 -0400

    move refrac_window to utils.py

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -2,7 +2,8 @@ from utils import (
     cast, detrend_linear, detrend_mean, detrend_none, dirsize, fractional,
     get_fft_funcs, group_indices, hascomplex, iscomplex, isvector, name2num,
     nans, ndlinspace, ndtuples, nextpow2, num2name, pad_larger, pad_larger2,
-    remove_legend, cartesian, compose, composemap, trimmean, nans_like)
+    remove_legend, cartesian, compose, composemap, trimmean, nans_like,
+    refrac_window)
 from span.utils.decorate import thunkify, cached_property
 from span.utils._clear_refrac import clear_refrac
 from span.utils._bin_data import bin_data
