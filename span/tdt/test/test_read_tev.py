3fdbd4998726fa35997ab275df1b3bbcfd4183ba
Phillip Cloud <cpcloud@gmail.com>
2012-10-21 17:48:45 -0400

    add test_read_tev test

---
+++ span/tdt/test/test_read_tev.py
@@ -0,0 +1,14 @@
+import unittest
+
+from span.tdt import read_tev
+
+class TestReadTev(unittest.TestCase):
+    """Test the Cython'ed read_tev function.
+    """
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def test_read_tev(self):
+        assert False
+
