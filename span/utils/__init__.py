6b8fb52b0ab0e4d55ee96b34973228dc91d10611
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:23:32 -0400

    export funcs

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -3,7 +3,7 @@ from utils import (
     get_fft_funcs, group_indices, hascomplex, iscomplex, isvector, name2num,
     nans, ndlinspace, ndtuples, nextpow2, num2name, pad_larger, pad_larger2,
     remove_legend, cartesian, compose, composemap, trimmean, nans_like,
-    refrac_window)
+    refrac_window, md5string, md5int, md5file)
 from span.utils.decorate import thunkify, cached_property
 from span.utils._clear_refrac import clear_refrac
 from span.utils._bin_data import bin_data
