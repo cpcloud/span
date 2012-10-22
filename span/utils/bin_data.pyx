668d0175bb3b1e99a9225cbcb8bfa1242559e5e0
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:23:50 -0400

    use cython memoryviews

--- span/utils/bin_data.pyx
+++ span/utils/bin_data.pyx
@@ -1,19 +1,19 @@
 """
 """
 
-# import numpy as np
+from numpy import asarray
+
 from numpy cimport (uint8_t as uint8, ndarray, int64_t as int64, PyArray_EMPTY,
                     NPY_LONG, npy_intp, import_array)
 
-from cython cimport wraparound, boundscheck
+cimport cython
 from cython.parallel cimport parallel, prange
 
 import_array()
 
-@wraparound(False)
-@boundscheck(False)
-cdef void _bin_data(ndarray[uint8, ndim=2, cast=True] a, ndarray[int64] bins,
-                    ndarray[int64, ndim=2] out):
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cdef void _bin_data(uint8[:, :] a, int64[:] bins, int64[:, :] out) nogil:
     """Sum the counts of spikes in `a` in each of the bins.
 
     Parameters
@@ -25,26 +25,21 @@ cdef void _bin_data(ndarray[uint8, ndim=2, cast=True] a, ndarray[int64] bins,
     cdef:
         int64 i, j, k, v
         int64 m = out.shape[0], n = out.shape[1]
-        int64* out_data = NULL
-        int64* bin_data = NULL
-        uint8* a_data = NULL    
 
-    with nogil, parallel():
-        out_data = <int64*> out.data
-        bin_data = <int64*> bins.data
-        a_data = <uint8*> a.data
-        
+    with parallel():
         for k in prange(n, schedule='guided'):
             for i in xrange(m):
                 v = 0
-                for j in prange(bin_data[i], bin_data[i + 1], schedule='guided'):
-                    v += a_data[j * n + k]
-                out_data[i * n + k] = v
+
+                for j in prange(bins[i], bins[i + 1], schedule='guided'):
+                    v += a[j, k]
+
+                out[i, k] = v
 
 
-@wraparound(False)
-@boundscheck(False)
-cpdef bin_data(ndarray[uint8, ndim=2, cast=True] a, ndarray[int64] bins):
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cpdef bin_data(uint8[:, :] a, int64[:] bins):
     """Wrapper around bin_data._bin_data.
 
     Parameters
@@ -63,7 +58,7 @@ cpdef bin_data(ndarray[uint8, ndim=2, cast=True] a, ndarray[int64] bins):
      # = np.empty((bins.shape[0] - 1, a.shape[1]),
                                                # dtype=np.int64)
     cdef npy_intp dims[2]
-    cdef ndarray[int64, ndim=2] out
+    cdef int64[:, :] out
 
     dims[0] = bins.shape[0] - 1
     dims[1] = a.shape[1]
@@ -72,4 +67,4 @@ cpdef bin_data(ndarray[uint8, ndim=2, cast=True] a, ndarray[int64] bins):
 
     _bin_data(a, bins, out)
 
-    return out
+    return asarray(out)
