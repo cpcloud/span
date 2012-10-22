c1de264eaa453a68c6a4ac1f128424566bb2f1a8
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:22:20 -0400

    add docstring; use memoryviews

--- span/tdt/read_tev.pyx
+++ span/tdt/read_tev.pyx
@@ -1,59 +1,74 @@
-from numpy cimport float32_t as float32, int64_t as int64, ndarray
+import os
+
+from numpy cimport float32_t as float32, int64_t as int64
 
 from libc.stdio cimport fopen, fclose, fread, fseek, SEEK_SET, FILE
 from libc.stdlib cimport malloc, free
+from libc.string cimport strlen
 
 from cython.parallel cimport prange, parallel
-from cython cimport boundscheck, wraparound
+cimport cython
 
 
-@boundscheck(False)
-@wraparound(False)
-cpdef read_tev(char* filename, int64 nsamples, ndarray[int64] fp_locs,
-               ndarray[float32, ndim=2] spikes):
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef read_tev(char* filename, int64 nsamples, int64[:] fp_locs,
+               float32[:, :] spikes):
     """
+
+    Parameters
+    ----------
+    filename : char *
+    nsamples : int64
+    fp_locs : int64[:]
+    spikes : float32[:, :]
+
+    Raises
+    ------
+    AssertionError
+        If filename is a NULL pointer
     """
-    assert filename, 'filename (1st argument) cannot be empty'
+    assert filename is not NULL, 'filename (1st argument) cannot be empty'
+    
+    _, ext = os.path.splitext(filename)
+    assert ext == 'tev', 'extension must be "tev"'
+    assert os.path.exists(filename), '%s does not exist' % filename
 
     cdef:
-        int64 i, j
-        int64 n = fp_locs.shape[0], nbytes = sizeof(float32)
+        int64 i, j, n = fp_locs.shape[0], nbytes = sizeof(float32)
 
-        float32* spikes_data = NULL
-        float32* chunk_data = NULL
-
-        int64* fp_locs_data = NULL
+        float32* chunk = NULL
 
         FILE* f = NULL
 
     with nogil, parallel():
-        spikes_data = <float32*> spikes.data
-        chunk_data = <float32*> malloc(nbytes * nsamples)
-        fp_locs_data = <int64*> fp_locs.data
+        chunk = <float32*> malloc(nbytes * nsamples)
 
         f = fopen(filename, 'rb')
 
         if not f:
-            free(chunk_data)
-            chunk_data = NULL
+            if chunk:
+                free(chunk)
+                chunk = NULL
 
             with gil:
                 raise IOError('Unable to open file %s' % filename)
 
         for i in prange(n, schedule='guided'):
             # go to the ith file pointer location
-            fseek(f, fp_locs_data[i], SEEK_SET)
+            fseek(f, fp_locs[i], SEEK_SET)
 
             # read nbytes * nsamples bytes into chunk_data
-            fread(chunk_data, nbytes, nsamples, f)
+            fread(chunk, nbytes, nsamples, f)
 
             # assign the chunk data to the spikes array
             for j in prange(nsamples, schedule='guided'):
-                spikes_data[i * nsamples + j] = chunk_data[j]
+                spikes[i, j] = chunk[j]
 
         # get rid of the chunk data
-        free(chunk_data)
-        chunk_data = NULL
+        if chunk:
+            free(chunk)
+            chunk = NULL
 
         fclose(f)
         f = NULL
