4d1063d16aa2f652c721740ef5c4fb8ffc7fd4f6
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:26:44 -0400

    use memoryviews

--- span/utils/clear_refrac.pyx
+++ span/utils/clear_refrac.pyx
@@ -1,21 +1,26 @@
-from numpy cimport uint8_t as uint8, int64_t as int64, ndarray
-from cython cimport boundscheck, wraparound
+from numpy cimport uint8_t as uint8, int64_t as int64
+cimport cython
 
 
-@wraparound(False)
-@boundscheck(False)
-cpdef clear_refrac(ndarray[uint8, ndim=2, cast=True] a, int64 window):
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cdef void _clear_refrac(uint8[:, :] a, int64 window) nogil:
     cdef:
         int64 channel, i, sample
         int64 nsamples = a.shape[0], nchannels = a.shape[1]
-        uint8* a_data = <uint8*> a.data
 
     for channel in xrange(nchannels):
         sample = 0
 
         while sample + window < nsamples:
-            if a_data[sample * nchannels + channel]:
+            if a[sample, channel]:
                 for i in xrange(sample + 1, sample + window + 1):
-                    a_data[i * nchannels + channel] = 0
+                    a[i, channel] = 0
                 sample += window
             sample += 1
+
+
+@cython.wraparound(False)
+@cython.boundscheck(False)
+cpdef clear_refrac(uint8[:, :] a, int64 window):
+    _clear_refrac(a, window)
