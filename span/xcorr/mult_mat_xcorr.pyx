22c532afd2148e016c19a8fb4c744339132a9874
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:28:38 -0400

    add non null assertions; use memoryviews

--- span/xcorr/mult_mat_xcorr.pyx
+++ span/xcorr/mult_mat_xcorr.pyx
@@ -1,33 +1,31 @@
-from numpy cimport complex128_t as complex128, int64_t as int64, ndarray
+from numpy cimport complex128_t as complex128, int64_t as int64
 
 from cython.parallel cimport parallel, prange
-from cython cimport boundscheck, wraparound
+cimport cython
 
 
-@wraparound(False)
-@boundscheck(False)
-cpdef mult_mat_xcorr(ndarray[complex128, ndim=2] X,
-                     ndarray[complex128, ndim=2] Xc,
-                     ndarray[complex128, ndim=2] c, int64 n, int64 nx):
+@cython.wraparound(False)
+@cython.boundscheck(False)
+def mult_mat_xcorr(complex128[:, :] X, complex128[:, :] Xc, complex128[:, :] c,
+                   int64 n, int64 nx):
     """Perform the necessary matrix-vector multiplication and fill the cross-
     correlation array. Slightly faster than pure Python.
 
     Parameters
     ----------
-    X, Xc, c : array
-    n : int64
+    X, Xc, c : array_like
+    n, nx : int64
     """
-    cdef:
-        int64 i, j, k, r
-        complex128 *c_data = NULL, *X_data = NULL, *Xc_data = NULL
+    assert X is not None
+    assert Xc is not None
+    assert c is not None
+    assert n > 0
+    assert nx > 0
+
+    cdef int64 i, j, k, r
 
     with nogil, parallel():
-        c_data = <complex128*> c.data
-        X_data = <complex128*> X.data
-        Xc_data = <complex128*> Xc.data
-
         for i in prange(n, schedule='guided'):
             for r, j in enumerate(xrange(i * n, (i + 1) * n)):
                 for k in prange(nx, schedule='guided'):
-                    c_data[j * nx + k] = (X_data[i * nx + k] *
-                                          Xc_data[r * nx + k])
+                    c[j, k] = X[i, k] * Xc[r, k]
