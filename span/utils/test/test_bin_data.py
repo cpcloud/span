f0b76d9fdbcdaafdd505ba1e2958bd4a900f8707
Phillip Cloud <cpcloud@gmail.com>
2012-10-22 10:30:00 -0400

    remove test_bin_data into a TestCase

--- span/utils/test/test_bin_data.py
+++ span/utils/test/test_bin_data.py
@@ -1,3 +1,5 @@
+import unittest
+
 from nose.tools import nottest
 
 import numpy as np
@@ -6,10 +8,11 @@ from numpy.random import rand, randint
 from span.utils import bin_data, cast
 
 
-def test_bin_data_none():
-    x = rand(randint(25, 200), randint(10, 20)) > 0.5
-    binsize = randint(10, max(x.shape))
-    bins = cast(np.r_[:binsize:x.shape[0]], int)
-    binned = bin_data(x, bins)
-    assert binned.shape == (bins.shape[0] - 1, x.shape[1])
-    assert binned.dtype == np.int64
+class TestBinDataNone(unittest.TestCase):
+    def test_bin_data_none(self):
+        x = rand(randint(25, 200), randint(10, 20)) > 0.5
+        binsize = randint(10, max(x.shape))
+        bins = cast(np.r_[:binsize:x.shape[0]], int)
+        binned = bin_data(x, bins)
+        self.assertTupleEqual(binned.shape, (bins.shape[0] - 1, x.shape[1]))
+        self.assertEqual(binned.dtype, np.int64)
