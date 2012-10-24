73e1cac67277e51c8211008002014c5784c3ccd0
Phillip Cloud <cpcloud@gmail.com>
2012-10-24 16:16:18 -0400

    parallel file read

--- span/tdt/read_tev.pyx
+++ span/tdt/read_tev.pyx
@@ -1,69 +1,82 @@
 import os
 
-from numpy cimport float32_t as float32, int64_t as int64
+from numpy cimport float32_t as f4, int64_t as i8
 
 from libc.stdio cimport fopen, fclose, fread, fseek, SEEK_SET, FILE
 from libc.stdlib cimport malloc, free
+
+from cython.parallel cimport parallel, prange
 
 cimport cython
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef void _read_tev(char* filename, int64 nsamples, int64[:] fp_locs,
-                    float32[:, :] spikes) nogil:
-    """
+cdef void _read_tev(char* filename, i8 nsamples, i8[:] fp_locs,
+                    f4[:, :] spikes) nogil:
+    """Read a TDT tev file in. Slightly faster than the pure Python version.
 
     Parameters
     ----------
     filename : char *
-    nsamples : int64
-    fp_locs : int64[:]
-    spikes : float32[:, :]
+        Name of the TDT file to load.
+
+    nsamples : i8
+        The number of samples per chunk of data.
+
+    fp_locs : i8[:]
+        The array of locations of each chunk in the TEV file.
+
+    spikes : f4[:, :]
+        Output array
     """
         
     cdef:
-        int64 i, j, n = fp_locs.shape[0], nbytes = sizeof(float32)
+        i8 i, j, n = fp_locs.shape[0], nbytes = sizeof(f4)
 
-        float32* chunk = <float32*> malloc(nbytes * nsamples)
+        f4* chunk = NULL
 
-        FILE* f = fopen(filename, 'rb')
+        FILE* f = NULL
 
-    if not f:
+    with parallel():
+        chunk = <f4*> malloc(nbytes * nsamples)
+        f = fopen(filename, 'rb')
+
+        if not f:
+            if chunk:
+                free(chunk)
+                chunk = NULL
+
+            with gil:
+                assert not chunk, 'memory leak when freeing chunk'
+                raise IOError('Unable to open file %s' % filename)
+
+        for i in prange(n, schedule='guided'):
+            # go to the ith file pointer location
+            fseek(f, fp_locs[i], SEEK_SET)
+
+            # read nbytes * nsamples bytes into chunk_data
+            fread(chunk, nbytes, nsamples, f)
+
+            # assign the chunk data to the spikes array
+            for j in xrange(nsamples):
+                spikes[i, j] = chunk[j]
+
+        # get rid of the chunk data
         if chunk:
             free(chunk)
             chunk = NULL
 
-        with gil:
-            assert not chunk, 'memory leak when freeing chunk'
-            raise IOError('Unable to open file %s' % filename)
+            with gil:
+                assert not chunk, 'memory leak when freeing chunk'
 
-    for i in xrange(n):
-        # go to the ith file pointer location
-        fseek(f, fp_locs[i], SEEK_SET)
-
-        # read nbytes * nsamples bytes into chunk_data
-        fread(chunk, nbytes, nsamples, f)
-
-        # assign the chunk data to the spikes array
-        for j in xrange(nsamples):
-            spikes[i, j] = chunk[j]
-
-    # get rid of the chunk data
-    if chunk:
-        free(chunk)
-        chunk = NULL
-
-        with gil:
-            assert not chunk, 'memory leak when freeing chunk'
-
-    fclose(f)
-    f = NULL
+        fclose(f)
+        f = NULL
 
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-def read_tev(char* filename, int64 nsamples, int64[:] fp_locs not None,
-             float32[:, :] spikes not None):
-    assert filename is not NULL, 'filename (1st argument) cannot be empty'
+def read_tev(char* filename, i8 nsamples, i8[:] fp_locs not None,
+             f4[:, :] spikes not None):
+    assert filename, 'filename (1st argument) cannot be empty'
     _read_tev(filename, nsamples, fp_locs, spikes)
