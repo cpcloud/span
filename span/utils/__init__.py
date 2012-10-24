b2d1995c190347011d7e9a13f34aeafff3f966e6
Phillip Cloud <cpcloud@gmail.com>
2012-10-24 16:09:04 -0400

    export newly named function

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -2,8 +2,8 @@ from utils import (
     cast, detrend_linear, detrend_mean, detrend_none, dirsize, fractional,
     get_fft_funcs, group_indices, hascomplex, iscomplex, isvector, name2num,
     nans, ndlinspace, ndtuples, nextpow2, num2name, pad_larger, pad_larger2,
-    remove_legend, cartesian, compose, composemap, trimmean, nans_like,
-    refrac_window, md5string, md5int, md5file)
+    remove_legend, cartesian, compose, composemap, trimmean, nans_like, fs2ms,
+    md5string, md5int, md5file, index_values)
 from span.utils.decorate import thunkify, cached_property
 from span.utils._clear_refrac import clear_refrac
 from span.utils._bin_data import bin_data
