2ab4cb76598edc509e58e937476d2fd04370e2f0
Phillip Cloud <cpcloud@gmail.com>
2012-10-27 16:22:29 -0400

    move nogil inside function; slow when used in function spec

--- span/utils/clear_refrac.pyx
+++ span/utils/clear_refrac.pyx
@@ -4,18 +4,19 @@ cimport cython
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef void _clear_refrac(u1[:, :] a, i8 window) nogil:
+cdef void _clear_refrac(u1[:, :] a, i8 window):
     cdef i8 channel, i, sample, nsamples = a.shape[0], nchannels = a.shape[1]
 
-    for channel in xrange(nchannels):
-        sample = 0
+    with nogil:
+        for channel in xrange(nchannels):
+            sample = 0
 
-        while sample + window < nsamples:
-            if a[sample, channel]:
-                for i in xrange(sample + 1, sample + window + 1):
-                    a[i, channel] = 0
-                sample += window
-            sample += 1
+            while sample + window < nsamples:
+                if a[sample, channel]:
+                    for i in xrange(sample + 1, sample + window + 1):
+                        a[i, channel] = 0
+                    sample += window
+                sample += 1
 
 
 @cython.wraparound(False)
