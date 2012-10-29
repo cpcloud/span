f56ae125b643867268c51fb5b68c71c2a7d0765e
Phillip Cloud <cpcloud@gmail.com>
2012-10-29 15:22:30 -0400

    setup.py now builds exts in correct folder so no more repetitive re-building of Cython exts

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 
-from future_builtins import zip
+from future_builtins import zip, map
 
 import os
-import platform
 import glob
+import warnings
 
 try:
     import numpy as np
@@ -19,29 +19,35 @@ from Cython.Distutils.extension import Extension
 extra_compile_args = ['-fopenmp']
 extra_link_args = ['-fopenmp']
 
-if platform.system().lower() == 'linux':
-    extra_compile_args.append('-march=native')
-
 npy_includes = np.get_include()
 include_dirs = [npy_includes]
 
 # define_macros = [('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
 define_macros = []
 
-utils_dir = os.path.join('span', 'utils')
-xcorr_dir = os.path.join('span', 'xcorr')
-tdt_dir = os.path.join('span', 'tdt')
+# module file names sans extension
+names = 'clear_refrac', 'bin_data', 'mult_mat_xcorr', 'read_tev'
 
+# module packages
+mod_pkgs = 'span.utils', 'span.utils', 'span.xcorr', 'span.tdt'
 
-base_names = 'clear_refrac', 'bin_data', 'mult_mat_xcorr', 'read_tev'
-dirs = utils_dir, utils_dir, xcorr_dir, tdt_dir
+# prefix for *.so files
+prefix = '_'
+
+# name of python module as if one was going to import it
+base_names = tuple(map(lambda x, y: os.extsep.join((x, prefix + y)), mod_pkgs, names))
+
+# directory of the modules/files
+dirs = map(lambda x: x.replace(os.extsep, os.sep), mod_pkgs)
 
 ext_modules = []
 
+
 for d, base_name in zip(dirs, base_names):
-    ext_modules.append(Extension('_%s' % base_name,
-                                 [os.path.join(d, '{0}{1}pyx'.format(base_name,
-                                                                     os.extsep))],
+    pyx_file_base = base_name.split(os.extsep)[-1].lstrip(prefix)
+    pyx_file = os.path.join(d, pyx_file_base + os.extsep + 'pyx')
+
+    ext_modules.append(Extension(base_name, [pyx_file],
                                  define_macros=define_macros,
                                  extra_compile_args=extra_compile_args,
                                  extra_link_args=extra_link_args,
@@ -49,9 +55,28 @@ for d, base_name in zip(dirs, base_names):
 
 
 if __name__ == '__main__':
-    readme_filename = glob.glob('README*')[0]
-    with open(readme_filename, 'r') as f:
-        readme = f.read()
+    readme_filename = glob.glob('README*')
+
+    nr = len(readme_filename)
+
+    if nr > 1 or not nr:
+        if nr > 1:
+            msg = 'More than one README* file found, '\
+                  'using first. Found {0}'.format(readme_filename)
+            readme_filename = readme_filename[0]
+        else:
+            msg = 'No README* file(s) found, leaving blank'
+            readme_filename = ''
+
+        warnings.warn(msg)
+
+
+    if readme_filename:
+        with open(readme_filename, 'r') as f:
+            readme = f.read()
+    else:
+        readme = ''
+
     setup(name='span',
           version='0.1',
           author='Phillip Cloud',
