ad2fcd3cec62256b2268612ed89983bb4a5b2b64
Phillip Cloud <cpcloud@gmail.com>
2012-10-29 13:57:51 -0400

    expose events and data types series

--- span/tdt/__init__.py
+++ span/tdt/__init__.py
@@ -1,5 +1,6 @@
 from span.tdt.spikedataframe import SpikeDataFrame
-from span.tdt.spikeglobals import Indexer, ShankMap, SortedIndexer, ChannelIndex
+from span.tdt.spikeglobals import (Indexer, ShankMap, SortedIndexer, ChannelIndex,
+                                   EventTypes, DataTypes)
 from span.tdt.tank import PandasTank
 from span.tdt.recording import distance_map, ElectrodeMap
 from span.tdt._read_tev import read_tev
