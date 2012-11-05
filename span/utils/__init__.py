99839fea7f5f31b37351783fcb029267e1745d6f
Phillip Cloud <cpcloud@gmail.com>
2012-11-05 15:17:32 -0500

    use star import for ease

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -1,9 +1,5 @@
-from utils import (
-    cast, detrend_linear, detrend_mean, detrend_none, dirsize, fractional,
-    get_fft_funcs, group_indices, hascomplex, iscomplex, isvector, name2num,
-    nans, ndlinspace, ndtuples, nextpow2, num2name, pad_larger, pad_larger2,
-    remove_legend, cartesian, compose, composemap, trimmean, nans_like, fs2ms,
-    md5string, md5int, md5file, index_values)
-from span.utils.decorate import thunkify, cached_property
-from span.utils._clear_refrac import clear_refrac
-from span.utils._bin_data import bin_data
+from utils import *
+from span.utils.math import *
+from span.utils.decorate import *
+from span.utils._clear_refrac import *
+from span.utils._bin_data import *
