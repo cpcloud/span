48deee324d7414f1b6cdb7512d5ebfa5be3bdffd
Phillip Cloud <cpcloud@gmail.com>
2012-11-09 10:44:45 -0500

    DOC/ENH: move doc to python func

--- span/xcorr/mult_mat_xcorr.pyx
+++ span/xcorr/mult_mat_xcorr.pyx
@@ -7,18 +7,12 @@ cimport cython
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-cdef void _mult_mat_xcorr(c16[:, :] X, c16[:, :] Xc, c16[:, :] c, i8 n, i8 nx):
-    """Perform the necessary matrix-vector multiplication and fill the cross-
-    correlation array. Slightly faster than pure Python.
+cdef void _mult_mat_xcorr(c16[:, :] X, c16[:, :] Xc, c16[:, :] c, i8 n,
+                          i8 nx) nogil:
 
-    Parameters
-    ----------
-    X, Xc, c : array_like
-    n, nx : i8
-    """
     cdef i8 i, j, k, r
 
-    with nogil, parallel():
+    with parallel():
         for i in prange(n, schedule='guided'):
             for r, j in enumerate(xrange(i * n, (i + 1) * n)):
                 for k in xrange(nx):
@@ -29,6 +23,20 @@ cdef void _mult_mat_xcorr(c16[:, :] X, c16[:, :] Xc, c16[:, :] c, i8 n, i8 nx):
 @cython.boundscheck(False)
 def mult_mat_xcorr(c16[:, :] X not None, c16[:, :] Xc not None,
                    c16[:, :] c not None, i8 n, i8 nx):
+    """Perform the necessary matrix-vector multiplication and fill the cross-
+    correlation array. Slightly faster than pure Python.
+
+    Parameters
+    ----------
+    X, Xc, c : c16[:, :]
+    n, nx : i8
+
+    Raises
+    ------
+    AssertionError
+       If n <= 0 or nx <= 0
+    """
     assert n > 0, 'n must be greater than 0'
     assert nx > 0, 'nx must be greater than 0'
+
     _mult_mat_xcorr(X, Xc, c, n, nx)
