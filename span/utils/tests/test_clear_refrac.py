2644606c66a75ac8951aa7860a51150b42361a4e
Phillip Cloud <cpcloud@gmail.com>
2012-11-12 08:55:41 -0500

    CLN: fix testse

--- span/utils/tests/test_clear_refrac.py
+++ span/utils/tests/test_clear_refrac.py
@@ -16,14 +16,23 @@ class TestClearRefracModule(unittest.TestCase):
         cls.x = rand(m, n)
         cls.window = int(np.floor(ms / 1e3 * fs))
 
+        if not cls.window or cls.window < 0:
+            cls.window = abs(cls.window) + 1
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.window, cls.x
+
     def test_many_threshes(self):
         thr = self.x > rand(self.x.shape[1])
         cleared = thr.copy()
-        clear_refrac(thr, self.window)
+        clear_refrac(thr.view(np.uint8), self.window)
+        self.assertRaises(ValueError, clear_refrac, thr, self.window)
         self.assertFalse(np.array_equal(thr, cleared))
 
     def test_one_thresh(self):
         thr = self.x > rand()
         cleared = thr.copy()
-        clear_refrac(thr, self.window)
+        clear_refrac(thr.view(np.uint8), self.window)
+        self.assertRaises(ValueError, clear_refrac, thr, self.window)
         self.assertFalse(np.array_equal(thr, cleared))
