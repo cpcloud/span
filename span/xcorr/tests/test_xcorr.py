cf9189e8cccec66c03567c4bd11d55f404e79352
Phillip Cloud <cpcloud@gmail.com>
2012-11-16 13:40:47 -0500

    TST/CLN: use setUp instead of setUpClass finish up tests for np correlate comparison

--- span/xcorr/tests/test_xcorr.py
+++ span/xcorr/tests/test_xcorr.py
@@ -1,6 +1,8 @@
 import unittest
+import itertools
 
 import numpy as np
+from numpy.random import randn, randint
 from numpy.testing import assert_allclose
 
 from span.xcorr import mult_mat_xcorr
@@ -9,16 +11,14 @@ from span.utils import nextpow2, get_fft_funcs
 
 
 class TestXCorr(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.m = 100
-        cls.n = 50
-        cls.matrix = np.random.randn(cls.m, cls.n)
-        cls.vector = cls.matrix[:, np.random.randint(cls.n)]
+    def setUp(self):
+        self.m = 100
+        self.n = 50
+        self.matrix = randn(self.m, self.n)
+        self.vector = self.matrix[:, randint(self.n)]
 
-    @classmethod
-    def tearDownClass(cls):
-        del cls.vector, cls.matrix, cls.m, cls.n
+    def tearDown(self):
+        del self.vector, self.matrix, self.m, self.n
 
     def assert_matrix_shape(self, c):
         self.assertEqual(c.ndim, 2)
@@ -31,7 +31,7 @@ class TestXCorr(unittest.TestCase):
     def assert_vector_shape(self, v):
         self.assertEqual(v.ndim, 1)
 
-        m, = v.shape
+        m = v.shape[0]
 
         self.assertEqual(m, 2 * self.m - 1)
 
@@ -45,7 +45,9 @@ class TestXCorr(unittest.TestCase):
 
             v = xcorr(self.vector, scale_type=scale_type)
 
-            vbig, vsmall = np.random.randn(self.m), np.random.randn(self.m - 20)
+            vbig = randn(self.m)
+            vsmall = randn(self.m - 20)
+
             vbs = xcorr(vbig, vsmall, scale_type=scale_type)
             self.assert_vector_shape(vbs)
 
@@ -62,8 +64,9 @@ class TestXCorr(unittest.TestCase):
             self.assertRaises(AssertionError, xcorr, self.vector,
                               scale_type=scale_type,
                               maxlags=self.vector.size + 10)
-            self.assertRaises(AssertionError, xcorr, vbig, scale_type=scale_type,
-                              maxlags=vbig.size + 10)
+            self.assertRaises(AssertionError, xcorr, vbig,
+                              scale_type=scale_type, maxlags=vbig.size + 10)
+
             if scale_type == 'normalize':
                 _, nc = c.shape
                 ncsqrt = int(np.sqrt(nc))
@@ -71,13 +74,13 @@ class TestXCorr(unittest.TestCase):
 
                 # lag 0s must be 1.0 for normalized
                 assert_allclose(c.ix[0, jkl], 1.0)
-                assert v.ix[0] == 1.0
+                self.assertEqual(v.ix[0], 1.0)
 
         def test_vector_vs_numpy_correlate(self):
             n = 10
             x, y = map(randn, itertools.repeat(n))
             xc_np = np.correlate(x, y, mode='full')
-            xc_span = span.xcorr.xcorr(x, y, detrend=None, scale_type=None)
+            xc_span = xcorr(x, y, detrend=None, scale_type=None).values
 
             assert_allclose(xc_np, xc_span.values)
 
@@ -86,14 +89,15 @@ class TestXCorr(unittest.TestCase):
             x = randn(m, n)
 
             xc_np = np.zeros((2 * m - 1, n ** 2))
+            rng = xrange(n)
 
-            for ci, i, j in zip(itertools.count(), xrange(n), xrange(n)):
+            for ci, i, j in zip(itertools.count(), rng, rng):
                 xi, xj = x[:, i], x[:, j]
                 xc_np[:, ci] = np.correlate(xi, xj, mode='full')
 
-            xc_span = span.xcorr.xcorr(x, detrend=None, scale_type=None)
+            xc_span = xcorr(x, detrend=None, scale_type=None).values
 
-            assert_allclose(xc_np, xc_span.values)
+            assert_allclose(xc_np, xc_span)
 
 
 def test_mult_mat_xcorr():
