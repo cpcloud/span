220386e71e1964a6c72fcafac9481c83fb65c410
Phillip Cloud <cpcloud@gmail.com>
2012-11-25 14:10:56 -0500

    TST/CLN: rename testing.utils to testing.testing

---
+++ span/testing/testing.py
@@ -0,0 +1,32 @@
+from functools import wraps
+import nose
+
+from numpy.testing import assert_allclose, assert_array_equal
+from numpy.testing.decorators import slow
+
+from nose.tools import nottest
+from nose import SkipTest
+
+
+from numpy.random import uniform as randrange
+
+
+def assert_all_dtypes(df, dtype, msg='dtypes not all the same'):
+    assert all(dt == dtype for dt in df.dtypes), msg
+
+
+def skip(test):
+    @wraps(test)
+    def wrapper():
+        if mock:
+            return test()
+
+        raise nose.SkipTest
+
+
+def create_tsq():
+    pass
+
+
+def create_spike_dataframe():
+    pass
