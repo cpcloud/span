4d4c689e8527c3b4da9994289d1877cd9cd5c623
Phillip Cloud <cpcloud@gmail.com>
2012-11-26 18:49:01 -0500

    TST/CLN: remove unused imports use correct array assetion

--- span/tdt/tests/test_spikedataframe.py
+++ span/tdt/tests/test_spikedataframe.py
@@ -1,9 +1,7 @@
-
-import os
 import unittest
 import itertools
 
-# from nose.tools import nottest
+from nose.tools import nottest
 
 import numpy as np
 from numpy.random import randn, rand, randint
@@ -13,13 +11,10 @@ import pandas as pd
 from pandas import Series
 from pandas.util.testing import assert_frame_equal
 
-from span.tdt.tank import PandasTank
-from span.tdt.spikedataframe import (SpikeDataFrameBase, SpikeDataFrame,
-                                     SpikeGroupedDataFrame)
-from span.utils import (detrend_none, detrend_mean, detrend_linear,
-                        fromtimestamp)
-from span.testing import (assert_all_dtypes, randrange, create_spike_df,
-                          assert_allclose, assert_array_equal)
+
+from span.tdt.spikedataframe import (SpikeDataFrame, SpikeGroupedDataFrame)
+from span.utils import detrend_none, detrend_mean, detrend_linear
+from span.testing import assert_all_dtypes, create_spike_df, assert_array_equal
 
 
 class TestSpikeGrouper(unittest.TestCase):
@@ -73,48 +68,48 @@ class TestSpikeDataFrameBase(unittest.TestCase):
 
         nsamples, _ = tsq.shape[0], tsq.size.unique().max()
         size = nsamples * tsq.shape[1], tsq.channel.nunique()
-
         cls.rows, cls.columns = size
-        cls.raw = randrange(-5e-6, 5e-6, size=size)
 
     @classmethod
     def tearDownClass(cls):
-        del cls.raw, cls.columns, cls.rows, cls.meta, cls.df
+        del cls.columns, cls.rows, cls.meta, cls.df
 
     def setUp(self):
         self.spikes = self.df.copy()
+        self.raw = self.spikes.values.copy()
 
     def tearDown(self):
-        del self.spikes
+        del self.raw, self.spikes
 
     def test_init_noindex_nocolumns(self):
         spikes = SpikeDataFrame(self.raw, self.meta)
         self.assertRaises(AssertionError, SpikeDataFrame, self.raw, None)
         self.assertRaises(TypeError, SpikeDataFrame, self.raw)
-        assert_frame_equal(spikes, self.df)
+        assert_array_equal(spikes.values, self.raw)
 
     def test_init_noindex_columns(self):
         from span.tdt.spikeglobals import ChannelIndex as columns
         spikes = SpikeDataFrame(self.raw, self.meta, columns=columns)
         self.assertRaises(AssertionError, SpikeDataFrame, self.raw, None)
         self.assertRaises(TypeError, SpikeDataFrame, self.raw)
-        assert_frame_equal(spikes, self.df)
+        assert_array_equal(spikes.values, self.raw)
+        assert_array_equal(spikes.columns.values, columns.values)
 
     def test_init_with_index_nocolumns(self):
-        spikes = SpikeDataFrame(self.raw, self.meta, index=self.index)
-        assert_frame_equal(spikes, self.df)
+        spikes = SpikeDataFrame(self.raw, self.meta, index=self.spikes.index)
+        assert_array_equal(spikes.values, self.raw)
+        assert_array_equal(spikes.index.values, self.spikes.index.values)
 
     def test_init_with_index_with_columns(self):
         from span.tdt.spikeglobals import ChannelIndex as columns
-        spikes = SpikeDataFrame(self.raw, self.meta, index=self.index,
+        spikes = SpikeDataFrame(self.raw, self.meta, index=self.spikes.index,
                                 columns=columns)
         assert_frame_equal(spikes, self.df)
 
     def test_fs(self):
         fs = self.spikes.fs
-
         self.assertEqual(fs, self.meta.fs.max())
-        assert_array_equal(fs, self.meta.fs)
+        assert_array_equal([fs] * self.meta.fs.size, self.meta.fs)
 
         if hasattr(fs, 'dtype'):
             self.assert_(np.issubdtype(fs.dtype, np.floating))
@@ -178,7 +173,7 @@ class TestSpikeDataFrame(unittest.TestCase):
         args = itertools.product(binsizes, reject_counts, dropnas)
 
         for binsize, reject_count, dropna in args:
-            if binsize > 0 and reject_count > 0:
+            if binsize > 0 and reject_count >= 0:
                 if reject_count > 0:
                     binned = sp.bin(clr, binsize, reject_count, dropna)
             else:
@@ -191,9 +186,12 @@ class TestSpikeDataFrame(unittest.TestCase):
         thr = sp.threshold(3 * sp.std())
         clr = sp.clear_refrac(thr)
         binned = sp.bin(clr, binsize=1000)
+        axes = 0, 1
+        sems = True, False
         args = itertools.product(sp.columns.names,
-                                 xrange(len(sp.columns.names)), (0, 1),
-                                 (True, False))
+                                 xrange(len(sp.columns.names)),
+                                 axes,
+                                 sems)
 
         for level_name, level_number, axis, sem in args:
             for t in (level_name, level_number):
@@ -225,29 +223,27 @@ class TestSpikeDataFrame(unittest.TestCase):
         self.assertIsInstance(s_new, SpikeDataFrame)
         self.assertIsInstance(s, type(s_new))
 
-    # @nottest
+    @nottest
     @slow
     def test_xcorr(self):
-        thr = self.spikes.threshold(3 * self.spikes.std())
+        thr = self.spikes.threshold(3.0 * self.spikes.std())
         clr = self.spikes.clear_refrac(thr, ms=2)
-        binned = self.spikes.bin(clr, binsize=1000)
+        binned = self.spikes.bin(clr, binsize=100)
 
-        maxlags = None, 1, 100
+        maxlags = None, 1, 10
         detrends = detrend_mean, detrend_none, detrend_linear
         scale_types = None, 'none', 'normalize', 'unbiased', 'biased'
-        sortlevel_s = ('channel i', 'channel j', 'shank i', 'shank j',
+        sortlevels = ('channel i', 'channel j', 'shank i', 'shank j',
                        'side i', 'side j')
-        sortlevel_i = xrange(len(sortlevel_s))
+        sortlevels += tuple(xrange(len(sortlevels)))
         dropnas = True, False
         nan_autos = True, False
 
-        args = itertools.product(maxlags, detrends, scale_types, sortlevel_s,
-                                 sortlevel_i, dropnas, nan_autos)
+        args = itertools.product(maxlags, detrends, scale_types, sortlevels,
+                                 dropnas, nan_autos)
 
-        for (maxlag, detrend, scale_type, level_s, level_i, dropna,
-             nan_auto) in args:
-            for level in (level_s, level_i):
-                xc = self.spikes.xcorr(binned, maxlag, detrend, scale_type,
+        for maxlag, detrend, scale_type, dropna, level, nan_auto in args:
+            xc = self.spikes.xcorr(binned, maxlag, detrend, scale_type,
                                        level, dropna, nan_auto)
 
 
