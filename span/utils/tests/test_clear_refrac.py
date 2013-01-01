bbb728b9dd13d3010c976d17e0b5412dc2d44748
Phillip Cloud <cpcloud@gmail.com>
2013-01-01 16:51:19 -0500

    TST: make sure refractory period is at least 2 ms

--- span/utils/tests/test_clear_refrac.py
+++ span/utils/tests/test_clear_refrac.py
@@ -8,15 +8,15 @@ from span.utils import clear_refrac, nextpow2
 
 class TestClearRefracModule(unittest.TestCase):
     def setUp(self):
-        m, n = randint(10, 101), randint(4, 5)
-        ms = randint(2, 10)
+        m, n = randint(20, 101), randint(4, 5)
+        ms = randint(2, 11)
         fs = int(2 ** nextpow2(randint(1, m / ms)))
 
         self.x = rand(m, n)
         self.window = int(np.floor(ms / 1e3 * fs))
 
-        if not self.window or self.window < 0:
-            self.window = abs(self.window) + 1
+        if not self.window:
+            self.window += 1
 
     def tearDown(self):
         del self.window, self.x
