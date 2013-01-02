d0ae81b3da456e2c1fc4874d4b99fc4dc0888f75
Phillip Cloud <cpcloud@gmail.com>
2013-01-02 15:05:49 -0500

    TST: add testing

--- span/xcorr/tests/test_xcorr.py
+++ span/xcorr/tests/test_xcorr.py
@@ -15,23 +15,15 @@ from span.utils import (nextpow2, get_fft_funcs, detrend_none,
 
 
 class TestXCorr(unittest.TestCase):
-    def setUp(self):
-        self.m, self.n = 100, 50
-        self.matrix = randn(self.m, self.n)
-        self.vector = self.matrix[:, randint(self.n)]
-
-    def tearDown(self):
-        del self.vector, self.matrix, self.m, self.n
-
     def test_matrix_vs_numpy_correlate(self):
-        m, n = 20, 10
+        m, n = 3, 2
         x = randn(m, n)
 
         rng = xrange(n)
 
         detrends = detrend_mean, detrend_none, detrend_linear
         scale_types = 'normalize', None, 'unbiased', 'biased', 'none'
-        maxlags = None, 2, 20
+        maxlags = None, 2, 4
 
         args = cartprod(maxlags, detrends, scale_types)
 
@@ -39,6 +31,7 @@ class TestXCorr(unittest.TestCase):
         cart = cartesian((np.arange(n), np.arange(n)))
 
         cols = map(tuple, cart)
+
         for ml, dt, st in args:
             xc_np.fill(0)
 
@@ -115,10 +108,10 @@ class TestXCorr(unittest.TestCase):
                 assert_allclose(xc_s2, xc_s2_np)
 
     def test_numpy_matrix_input(self):
-        x = randn(10, 15)
+        x = randn(2, 3)
         detrends = detrend_mean, detrend_none, detrend_linear
-        scale_types = 'normalize', None, 'unbiased', 'biased', 'none'
-        maxlags = 8, None, 100
+        scale_types = 'normalize', 'unbiased', 'biased', 'none', None
+        maxlags = None, 2, 3
 
         args = cartprod(maxlags, detrends, scale_types)
 
@@ -131,35 +124,38 @@ class TestXCorr(unittest.TestCase):
                 self.assertIsInstance(xcnn, np.ndarray)
 
     def test_numpy_vector_input(self):
-        x = randn(10)
+        x = randn(2)
         detrends = detrend_mean, detrend_none, detrend_linear
         scale_types = 'normalize', 'none', 'unbiased', 'biased', None
-        maxlags = 10, None, 1000
+        maxlags = None, 2, 3
 
         args = cartprod(maxlags, detrends, scale_types)
 
         for ml, dt, st in args:
             y = randn(randint(10, 15))
 
-            if ml > max(x.shape + y.shape):
+            if ml > max(x.shape):
                 self.assertRaises(AssertionError, xcorr, x, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-                self.assertRaises(AssertionError, xcorr, x, y, detrend=dt,
                                   scale_type=st, maxlags=ml)
                 self.assertRaises(AssertionError, xcorr, x, y=None, detrend=dt,
                                   scale_type=st, maxlags=ml)
             else:
                 xcnn = xcorr(x, detrend=dt, scale_type=st, maxlags=ml)
-                xcnn2 = xcorr(x, y, detrend=dt, scale_type=st, maxlags=ml)
-                xcnn3 = xcorr(x, y=None, detrend=dt, scale_type=st, maxlags=ml)
-
                 self.assertIsInstance(xcnn, np.ndarray)
-                self.assertIsInstance(xcnn2, np.ndarray)
-                self.assertIsInstance(xcnn3, np.ndarray)
+
+                xcnn = xcorr(x, y=None, detrend=dt, scale_type=st, maxlags=ml)
+                self.assertIsInstance(xcnn, np.ndarray)
+
+            if ml > max(x.shape + y.shape):
+                self.assertRaises(AssertionError, xcorr, x, y, detrend=dt,
+                                  scale_type=st, maxlags=ml)
+            else:
+                xcnn = xcorr(x, y, detrend=dt, scale_type=st, maxlags=ml)
+                self.assertIsInstance(xcnn, np.ndarray)
 
 
 def test_mult_mat_xcorr():
-    x = randn(randint(50, 71), randint(2, 21))
+    x = randn(randint(2, 3), randint(2, 4))
     m, n = x.shape
     ifft, fft = get_fft_funcs(x)
     nfft = int(2 ** nextpow2(m))
