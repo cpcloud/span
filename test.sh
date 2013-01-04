3fab0d7a486faf6755431ac90d4df767f7e54cde
Phillip Cloud <cpcloud@gmail.com>
2013-01-04 14:49:25 -0500

    TST: add default coverage package

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ nosetests -w span --ignore='make_feature_file.py' --ignore='.*flymake.*' \
     --detailed-errors \
     --nocapture \
     --with-coverage \
-    --cover-package=span \
+    --cover-package="${1:-span}" \
     --cover-tests \
     --cover-erase \
     --cover-inclusive \
