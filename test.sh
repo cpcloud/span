4c184b319b4eac83557f711aa6627b2b50995264
Phillip Cloud <cpcloud@gmail.com>
2013-01-04 18:30:03 -0500

    TST: just use span for coverage

--- test.sh
+++ test.sh
@@ -2,9 +2,8 @@
 
 nosetests -w span --ignore='make_feature_file.py' --ignore='.*flymake.*' \
     --detailed-errors \
-    --nocapture \
     --with-coverage \
-    --cover-package="${1:-span}" \
+    --cover-package=span \
     --cover-tests \
     --cover-erase \
     --cover-inclusive \
