bed47ec773e6646152ab369831acb96407e94c83
Phillip Cloud <cpcloud@gmail.com>
2013-01-04 19:52:18 -0500

    TST: update move of read_tev tests accordingly

--- span/tdt/tests/test_tank.py
+++ span/tdt/tests/test_tank.py
@@ -3,14 +3,50 @@ import types
 import numbers
 import unittest
 import datetime
+from glob import glob
 
 import numpy as np
 import pandas as pd
 
-from span.tdt.tank import TdtTankBase, PandasTank
+from span.tdt.tank import TdtTankBase, PandasTank, _read_tev
 from span.tdt import SpikeDataFrame
 from span.tdt.tank import _get_first_match, _match_int
 from span.testing import slow
+
+
+@slow
+class TestReadTev(unittest.TestCase):
+    def setUp(self):
+        home = os.path.expanduser('~')
+        path = os.path.join(home, 'Data', 'xcorr_data',
+                            'Spont_Spikes_091210_p17rat_s4_657umV')
+        self.path = glob(os.path.join(path, '*%stev' % os.extsep))[0]
+        self.tank = PandasTank(self.path[:-4])
+        self.names = 'Spik', 'LFPs'
+
+    def tearDown(self):
+        del self.names, self.tank, self.path
+
+    def test_read_tev(self):
+        for name in self.names:
+            tsq, _ = self.tank.tsq(name)
+            fp_locs = tsq.fp_loc
+
+            self.assertEqual(np.dtype(np.int64), fp_locs.dtype)
+
+            nsamples, chunk_size = fp_locs.size, tsq.size.unique().max()
+
+            del tsq
+
+            spikes = np.empty((nsamples, chunk_size), np.float32)
+
+            _read_tev(self.path, chunk_size, fp_locs, spikes)
+
+            # mean should be at least on the order of millivolts if not less
+            mag = np.log10(np.abs(spikes).mean())
+            self.assertLessEqual(mag, -3.0)
+
+            del spikes, mag, fp_locs
 
 
 class TestGetFirstMatch(unittest.TestCase):
