69e82c6922618bbe73bf22f68e7f4d395dfd654e
Phillip Cloud <cpcloud@gmail.com>
2013-01-04 19:52:36 -0500

    CLN: remove unused imports

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -21,5 +21,4 @@
 from utils import *
 from span.utils.math import *
 from span.utils.decorate import *
-from span.utils._clear_refrac import *
-from span.utils._bin_data import *
+from span.utils import clear_refrac, bin_data
