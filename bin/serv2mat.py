d7b6477666937c1337a932bb3a873b445ecd6deb
Phillip Cloud <cpcloud@gmail.com>
2013-02-12 17:32:49 -0500

    debug

--- bin/serv2mat.py
+++ bin/serv2mat.py
@@ -64,6 +64,9 @@ def parse_args():
 
 def main():
     dn = parse_args().dirname.rstrip(os.sep)
+    print
+    print dn
+    print
     tev, = glob.glob(os.path.join(dn, '*.tev'))
     tev_name, _ = os.path.splitext(tev)
     mat_filename = os.path.join(tev_name + os.extsep + 'mat')
