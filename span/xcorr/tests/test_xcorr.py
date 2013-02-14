c21bfbdec0acb0aa46081322145153da935a1740
Phillip Cloud <cpcloud@gmail.com>
2013-02-14 14:36:21 -0500

    remove crap testing

--- span/xcorr/tests/test_xcorr.py
+++ span/xcorr/tests/test_xcorr.py
@@ -1,161 +1,92 @@
 
 import unittest
-from itertools import product as cartprod
-
-import ipdb
 
 import numpy as np
 from numpy.random import randn, randint
 from numpy.testing import assert_allclose
-from numpy import sum, abs
 
-from pandas import DataFrame, Int64Index, Series
+from pandas import DataFrame, Series
+from pandas.util.testing import assert_frame_equal
 
-from span.xcorr import mult_mat_xcorr
-from span.xcorr.xcorr import (xcorr, mult_mat_xcorr_numba,
+from span.xcorr import mult_mat_xcorr, xcorr
+from span.xcorr.xcorr import (mult_mat_xcorr_numba,
                               mult_mat_xcorr_cython_parallel,
                               mult_mat_xcorr_cython_serial,
                               mult_mat_xcorr_numba_sliced,
-                              mult_mat_xcorr_python)
+                              mult_mat_xcorr_python, crosscorr, autocorr,
+                              matrixcorr)
 from span.utils import (nextpow2, get_fft_funcs, detrend_none,
                         detrend_mean, detrend_linear, cartesian)
+from span.testing import assert_array_equal, knownfailure
+
+
+def is_symmetric(x):
+    return np.array_equal(x, x.T)
+
+
+def assert_symmetric(x):
+    methods = {DataFrame: assert_frame_equal,
+               np.ndarray: assert_array_equal}
+    method = methods[type(x)]
+    return method(x, x.T)
+
+
+def row(x):
+    m = x.shape[0]
+    return np.tile(np.r_[:m][:, np.newaxis], m)
+
+
+def col(x):
+    n = x.shape[1]
+    return np.tile(np.r_[:n][np.newaxis], (n, 1))
+
+
+def correlate1d(x, y):
+    xc = x - x.mean()
+    yc = y - y.mean()
+    z = np.sqrt(np.sum(np.abs(xc) ** 2) * np.sum(np.abs(yc) ** 2))
+    return np.correlate(xc, yc, mode='full') / z
 
 
 class TestXCorr(unittest.TestCase):
-    def test_matrix_vs_numpy_correlate(self):
-        m, n = 5, 3
-        x = randn(m, n)
+    def setUp(self):
+        self.m, self.n = 3, 2
+        self.x, self.y = randn(self.m), randn(self.n)
+        self.xsame, self.ysame = self.x, randn(self.m)
+        self.matrix = randn(self.m, self.n)
 
-        rng = xrange(n)
+    def test_autocorr(self):
+        x = self.x
+        npc = np.correlate(x, x, mode='full')
+        spc = xcorr(x)
+        assert_allclose(npc, spc)
 
-        detrends = detrend_mean, detrend_none, detrend_linear
-        scale_types = 'normalize', None, 'unbiased', 'biased'
-        maxlags = None, 2, 20
+    def test_crosscorr_same_lengths(self):
+        x, y = self.xsame, self.ysame
+        npc = np.correlate(x, y, mode='full')
+        spc = xcorr(x, y)
+        assert_allclose(npc, spc)
 
-        args = cartprod(maxlags, detrends, scale_types)
+    @knownfailure
+    def test_crosscorr_diff_lengths(self):
+        # this fails because np.correlate does something strange to
+        # the input: it removes the padding that it adds
+        # the span.xcorr.xcorr function replicates MATLAB's xcorr's behavior
+        x, y = self.x, self.y
+        npc = np.correlate(x, y, mode='full')
+        spc = xcorr(x, y)
+        assert_allclose(npc, spc)
 
-        xc_np = np.zeros((2 * m - 1, n ** 2))
-        cart = cartesian((np.arange(n), np.arange(n)))
+    def test_lag0_matrix(self):
+        x = self.matrix
+        df = DataFrame(x)
+        np_lag0 = np.corrcoef(x.T)
+        pd_lag0 = df.corr()
+        sp_lag0 = xcorr(df, detrend=detrend_mean, scale_type='normalize')
 
-        cols = map(tuple, cart)
-        for ml, dt, st in args:
-            xc_np.fill(0)
-
-            mml = ml if ml is not None else m
-
-            xc_span = DataFrame(xc_np.copy(), columns=cols,
-                                index=Int64Index(np.r_[1 - m:m]))
-
-            for ci, (i, j) in enumerate(cartprod(rng, rng)):
-                xi, xj = dt(x[:, i]), dt(x[:, j])
-
-                xc_np[:, ci] = np.correlate(xi, xj, mode='full')
-
-                if st == 'normalize':
-                    cxx0 = sum(abs(xi) ** 2)
-                    cyy0 = sum(abs(xj) ** 2)
-                    xc_np[:, ci] /= np.sqrt(cxx0 * cyy0)
-
-                elif st == 'biased':
-                    xc_np[:, ci] /= m
-
-                elif st == 'unbiased':
-                    xc_np[:, ci] /= m - np.abs(np.r_[1 - m:m])
-
-                xc_span[i, j] = xcorr(xi, xj, detrend=dt, scale_type=st)
-
-            if ml > m:
-                self.assertRaises(AssertionError, xcorr, x, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-            else:
-                xcc = xcorr(x, detrend=dt, scale_type=st, maxlags=ml)
-                assert_allclose(xc_np, xc_span)
-                dd = DataFrame(xc_np, index=np.r_[1 - m:m])
-
-                # funky pandas df indexing must subtract one because endpoints
-                # are inclusive
-                ipdb.set_trace()
-                assert_allclose(dd.ix[1 - mml:mml - 1].values, xcc)
-
-                self.assertIsInstance(xcc, np.ndarray)
-
-            if ml > m:
-                self.assertRaises(AssertionError, xcorr, DataFrame(x),
-                                  detrend=dt, scale_type=st, maxlags=ml)
-            else:
-                xccdf = xcorr(DataFrame(x), detrend=dt, scale_type=st,
-                              maxlags=ml)
-                assert_allclose(xccdf, xcc)
-                self.assertIsInstance(xccdf, DataFrame)
-
-            if ml > m:
-                self.assertRaises(AssertionError, xcorr, Series(x[:, 0]),
-                                  detrend=dt, scale_type=st, maxlags=ml)
-                self.assertRaises(AssertionError, xcorr, x[:, 0],
-                                  detrend=dt, scale_type=st, maxlags=ml)
-            else:
-                xc_s = xcorr(Series(x[:, 0]), detrend=dt, scale_type=st,
-                             maxlags=ml)
-                xc_s2 = xcorr(Series(x[:, 0]), y=None, detrend=dt,
-                              scale_type=st, maxlags=ml)
-
-                assert_allclose(xc_s, xc_s2)
-
-            if ml > m:
-                self.assertRaises(AssertionError, xcorr, Series(x[:, 0]),
-                                  Series(x[:, 1]), detrend=dt, scale_type=st,
-                                  maxlags=ml)
-                self.assertRaises(AssertionError, xcorr, x[:, 0], x[:, 1],
-                                  detrend=dt, scale_type=st, maxlags=ml)
-            else:
-                xc_s2 = xcorr(Series(x[:, 0]), Series(x[:, 1]), detrend=dt,
-                              scale_type=st, maxlags=ml)
-                xc_s2_np = xcorr(x[:, 0], x[:, 1], detrend=dt, scale_type=st,
-                                 maxlags=ml)
-                assert_allclose(xc_s2, xc_s2_np)
-
-    def test_numpy_matrix_input(self):
-        x = randn(2, 3)
-        detrends = detrend_mean, detrend_none, detrend_linear
-        scale_types = 'normalize', None, 'unbiased', 'biased'
-        maxlags = 2, None, 100
-
-        args = cartprod(maxlags, detrends, scale_types)
-
-        for ml, dt, st in args:
-            if ml > x.shape[0]:
-                self.assertRaises(AssertionError, xcorr, x, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-            else:
-                xcnn = xcorr(x, detrend=dt, scale_type=st, maxlags=ml)
-                self.assertIsInstance(xcnn, np.ndarray)
-
-    def test_numpy_vector_input(self):
-        x = randn(3)
-        detrends = detrend_mean, detrend_none, detrend_linear, None
-        scale_types = 'normalize', 'none', 'unbiased', 'biased', None
-        maxlags = 2, None, 100
-
-        args = cartprod(maxlags, detrends, scale_types)
-
-        for ml, dt, st in args:
-            y = randn(randint(2, 3))
-
-            if ml > max(x.shape + y.shape):
-                self.assertRaises(AssertionError, xcorr, x, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-                self.assertRaises(AssertionError, xcorr, x, y, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-                self.assertRaises(AssertionError, xcorr, x, y=None, detrend=dt,
-                                  scale_type=st, maxlags=ml)
-            else:
-                xcnn = xcorr(x, detrend=dt, scale_type=st, maxlags=ml)
-                xcnn2 = xcorr(x, y, detrend=dt, scale_type=st, maxlags=ml)
-                xcnn3 = xcorr(x, y=None, detrend=dt, scale_type=st, maxlags=ml)
-
-                self.assertIsInstance(xcnn, np.ndarray)
-                self.assertIsInstance(xcnn2, np.ndarray)
-                self.assertIsInstance(xcnn3, np.ndarray)
+        assert_allclose(np_lag0, pd_lag0)
+        assert_allclose(np_lag0.ravel(), sp_lag0.ix[0])
+        assert_allclose(pd_lag0.values.ravel(), sp_lag0.ix[0])
 
 
 class TestMultMatXcorr(unittest.TestCase):
