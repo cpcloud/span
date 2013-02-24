c7f728fa70e71d923562c7603cbe430620505633
Phillip Cloud <cpcloud@gmail.com>
2013-02-23 22:37:30 -0500

    move xcorr inds to xcorr.py

--- span/tdt/spikedataframe.py
+++ span/tdt/spikedataframe.py
@@ -35,7 +35,7 @@ import abc
 import functools
 
 import numpy as np
-from pandas import Series, DataFrame, MultiIndex
+from pandas import Series, DataFrame
 
 from span.xcorr import xcorr as _xcorr
 from span.utils import samples_per_ms, clear_refrac
@@ -65,10 +65,6 @@ class SpikeDataFrameBase(DataFrame):
 
     @abc.abstractmethod
     def clear_refrac(self, *args, **kwargs):
-        pass
-
-    @abc.abstractmethod
-    def xcorr(self, *args, **kwargs):
         pass
 
 
@@ -237,10 +233,8 @@ class SpikeDataFrame(SpikeDataFrameBase):
         xc = _xcorr(binned, maxlags=maxlags, detrend=detrend,
                     scale_type=scale_type)
 
-        xc.columns = _create_xcorr_inds(binned.columns)
-
         if nan_auto:
-            # slight hack for channel names
+            # slight HACK for channel names
             xc0 = xc.ix[0]
             names = xc0.index.names
             chi_ind = names.index('channel i')
@@ -267,74 +261,3 @@ class SpikeDataFrame(SpikeDataFrameBase):
 
 
 spike_xcorr = SpikeDataFrame.xcorr
-
-
-# TODO: hack to make it so nans are allowed when creating indices
-def _create_xcorr_inds(columns, index_start_string='i'):
-    """Create an appropriate index for cross correlation.
-
-    Parameters
-    ----------
-    columns : MultiIndex
-    index_start_string : basestring
-
-    Returns
-    -------
-    mi : MultiIndex
-
-    Notes
-    -----
-    I'm not sure if this is actually slick, or just insane seems like
-    functional idioms are so concise as to be confusing sometimes,
-    although maybe I'm just slow.
-
-    This absolutely does not handle the case where there are more than
-    52 levels in the index, because i haven't had the chance to think
-    about it yet.
-
-    The reduce etc code is equivalent to the following loop-based code:
-
-    .. code-block:: python
-
-        red = []
-        for inds in xrs:
-            s = ()
-
-            for i in inds:
-                s += columns[i]
-
-            red.append(s)
-    """
-    from string import ascii_letters as letters
-    from itertools import product, repeat, cycle, islice, imap
-
-    # number of columns
-    ncols = len(columns)
-
-    # number levels
-    nlevels = len(columns.levels)
-
-    # {0, ..., ncols - 1} ^ nlevels
-    xrs = product(*repeat(xrange(ncols), nlevels))
-
-    # see docstring
-    inner_reducer = lambda i, j: columns[i] + columns[j]
-    reducer = lambda inds: reduce(inner_reducer, inds)
-    all_inds = imap(reducer, xrs)
-
-    colnames = columns.names
-
-    # get the index of the starting index string provided
-    first_ind = letters.index(index_start_string)
-
-    # repeat endlessly
-    cycle_letters = cycle(letters)
-
-    # slice from the index of the first letter to that plus the number
-    # of names
-    sliced = islice(cycle_letters, first_ind, first_ind + len(colnames))
-
-    # alternate names and index letter
-    names = sorted(product(colnames, sliced), key=lambda x: x[-1])
-    names = imap(' '.join, names)
-    return MultiIndex.from_tuples(list(all_inds), names=list(names))
