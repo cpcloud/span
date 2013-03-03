abaa155add198036a9296c57d027addb0fc16c54
Phillip Cloud <cpcloud@gmail.com>
2013-03-03 12:53:10 -0500

    export create_repeating_multi_index function

--- span/utils/__init__.py
+++ span/utils/__init__.py
@@ -21,7 +21,8 @@
 from span.utils.utils import (name2num, ndtuples, iscomplex,
                               get_fft_funcs, isvector,
                               assert_nonzero_existing_file,
-                              clear_refrac, ispower2, fromtimestamp)
+                              clear_refrac, ispower2, fromtimestamp,
+                              create_repeating_multi_index)
 from span.utils.math import (detrend_none, detrend_mean, detrend_linear,
                              cartesian, nextpow2, samples_per_ms, compose,
                              compose2, composemap)
@@ -31,4 +32,4 @@ __all__ = ('name2num', 'ndtuples', 'iscomplex', 'get_fft_funcs', 'isvector',
            'assert_nonzero_existing_file', 'clear_refrac', 'ispower2',
            'thunkify', 'ispower2', 'detrend_none', 'detrend_mean',
            'detrend_linear', 'cartesian', 'nextpow2', 'samples_per_ms',
-           'compose', 'compose2', 'composemap')
+           'compose', 'compose2', 'composemap', 'create_repeating_multi_index')
