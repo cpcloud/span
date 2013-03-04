1ea8b593be4772fb40739149db58adca3cdeb264
Phillip Cloud <cpcloud@gmail.com>
2013-03-03 22:23:50 -0500

    change spikes to spik

--- span/tdt/tests/test_spikedataframe.py
+++ span/tdt/tests/test_spikedataframe.py
@@ -16,14 +16,14 @@ from span.testing import (assert_all_dtypes, create_spike_df,
 
 class TestSpikeDataFrame(object):
     def setUp(self):
-        self.spikes = create_spike_df()
-        self.raw = self.spikes.values
+        self.spik = create_spike_df()
+        self.raw = self.spik.values
         self.threshes = 2e-5, 3e-5
         self.mses = None, 2, 3
         self.binsizes = 0, 1, randint(10, 12)
 
     def tearDown(self):
-        del self.spikes
+        del self.spik
 
     def test_init_noindex_nocolumns(self):
         spikes = SpikeDataFrame(self.raw)
@@ -36,37 +36,37 @@ class TestSpikeDataFrame(object):
         assert_array_equal(spikes.columns.values, columns.values)
 
     def test_init_with_index_nocolumns(self):
-        spikes = SpikeDataFrame(self.raw, index=self.spikes.index)
+        spikes = SpikeDataFrame(self.raw, index=self.spik.index)
         assert_array_equal(spikes.values, self.raw)
-        assert_array_equal(spikes.index.values, self.spikes.index.values)
+        assert_array_equal(spikes.index.values, self.spik.index.values)
 
     def test_init_with_index_with_columns(self):
         from span.tdt.spikeglobals import ChannelIndex as columns
         cols, _ = columns.swaplevel(1, 0).sortlevel('shank')
-        spikes = SpikeDataFrame(self.raw, index=self.spikes.index,
+        spikes = SpikeDataFrame(self.raw, index=self.spik.index,
                                 columns=cols)
-        assert_frame_equal(spikes, self.spikes)
+        assert_frame_equal(spikes, self.spik)
 
     def test_fs(self):
-        fs = self.spikes.fs
+        fs = self.spik.fs
         assert isinstance(fs, float)
 
     def test_nchannels(self):
-        nchans = self.spikes.nchannels
-        cols = self.spikes.columns
+        nchans = self.spik.nchannels
+        cols = self.spik.columns
         values = cols.levels[cols.names.index('channel')].values
         assert nchans == values.max() + 1
 
     def test_nsamples(self):
-        nsamps = self.spikes.nsamples
-        assert nsamps == self.spikes.shape[0]
+        nsamps = self.spik.nsamples
+        assert nsamps == self.spik.shape[0]
 
-        fs = self.spikes.fs
-        expected = round((self.spikes.nsamples / fs) * fs)
+        fs = self.spik.fs
+        expected = round((self.spik.nsamples / fs) * fs)
         assert nsamps == expected
 
     def test_threshold_std_array(self):
-        sp = self.spikes
+        sp = self.spik
         std = sp.std()
         thresh = 2.0
         thr = sp.threshold(thresh * std)
@@ -74,39 +74,39 @@ class TestSpikeDataFrame(object):
         assert thr.shape == sp.shape
 
     def test_threshold_std_scalar(self):
-        sp = self.spikes
+        sp = self.spik
         std = sp.std()
         thr = sp.threshold(std.values[randint(std.size)])
         assert_all_dtypes(thr, np.bool_)
         assert thr.shape == sp.shape
 
     def test_threshold_array(self):
-        sp = self.spikes
+        sp = self.spik
         std = rand(sp.shape[1])
         thr = sp.threshold(2.0 * std)
         assert_all_dtypes(thr, np.bool_)
         assert thr.shape == sp.shape
 
     def test_threshold_scalar(self):
-        sp = self.spikes
+        sp = self.spik
         std = rand()
         thr = sp.threshold(2.0 * std)
         assert_all_dtypes(thr, np.bool_)
         assert thr.shape == sp.shape
 
     def test_clear_refrac(self):
-        thr = self.spikes.threshold(3 * self.spikes.std())
+        thr = self.spik.threshold(3 * self.spik.std())
 
         for ms in self.mses:
-            cleared = self.spikes.clear_refrac(thr, ms)
+            cleared = self.spik.clear_refrac(thr, ms)
             assert_all_dtypes(cleared, np.bool_)
             assert cleared.values.sum() <= thr.values.sum()
 
     def test_constructor(self):
-        s = self.spikes
-        ind = self.spikes.index
-        cols = self.spikes.columns
-        s_new = self.spikes._constructor(s.values, index=ind, columns=cols)
+        s = self.spik
+        ind = self.spik.index
+        cols = self.spik.columns
+        s_new = self.spik._constructor(s.values, index=ind, columns=cols)
         assert isinstance(s_new, pd.DataFrame)
         assert isinstance(s_new, SpikeDataFrame)
         assert isinstance(s, type(s_new))
@@ -128,22 +128,22 @@ class TestSpikeDataFrame(object):
                    nan_auto)
 
     def xcorr_builder(self, maxlag, detrend, scale_type, level, nan_auto):
-        thr = self.spikes.threshold(self.spikes.std())
-        clr = self.spikes.clear_refrac(thr)
+        thr = self.spik.threshold(self.spik.std())
+        clr = self.spik.clear_refrac(thr)
         binned = clr.resample('L', how='sum')
-        xc = self.spikes.xcorr(binned, maxlag, detrend, scale_type, level,
+        xc = self.spik.xcorr(binned, maxlag, detrend, scale_type, level,
                                nan_auto)
         assert isinstance(xc, pd.DataFrame)
 
         weird_levels = 'asdfalsdj', 2342, object()
 
         for level in weird_levels:
-            assert_raises(Exception, self.spikes.xcorr, binned, maxlag,
+            assert_raises(Exception, self.spik.xcorr, binned, maxlag,
                           detrend, scale_type, level, nan_auto)
-        assert_raises(AssertionError, self.spikes.xcorr, binned,
+        assert_raises(AssertionError, self.spik.xcorr, binned,
                       binned.shape[0] + 10, detrend, scale_type, level,
                       nan_auto)
 
     def test_jitter(self):
-        jittered = self.spikes.jitter()
-        assert not np.array_equal(jittered, self.spikes)
+        jittered = self.spik.jitter()
+        assert not np.array_equal(jittered, self.spik)
