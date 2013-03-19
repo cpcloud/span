69d8a3ded6c11da2fbf9f00e33c04d412e74d153
Phillip Cloud <cpcloud@gmail.com>
2013-03-19 14:59:12 -0400

    axis bug

--- bin/tdt2mat.py
+++ bin/tdt2mat.py
@@ -42,7 +42,7 @@ def convert_and_save(filename):
     base_filename, _ = os.path.splitext(filename)
 
     print '\nConverting TDT Tank to MATLAB: {0}'.format(base_filename)
-    sp = span.PandasTank(base_filename).spik.reorder_levels((1, 0))
+    sp = span.PandasTank(base_filename).spik.reorder_levels((1, 0), axis=1)
     sp.sort_index(axis=1, inplace=True)
     serv2mat(sp, base_filename)
     print 'Done!'
