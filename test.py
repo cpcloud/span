c58a2faf1b77007a055ad9de30b1a70e9bc419f5
Phillip Cloud <cpcloud@gmail.com>
2013-03-30 16:23:08 -0400

    new testing script

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    import argparse
+    import os
+    from multiprocessing import cpu_count
+    from nose import run
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--fast', action='store_true', default=False)
+    parser.add_argument('-t', '--cover-tests', action='store_true',
+                        default=False)
+    parser.add_argument('-c', '--cover', action='store_true', default=False)
+    parsed_args = parser.parse_args()
+
+    package_name = os.path.basename(os.path.dirname(os.curdir))
+
+    argv = ['nosetests', '-w', package_name]
+
+    if parsed_args.fast:
+        argv.extend(['-A "not slow"', '--processes=%d' % cpu_count()])
+
+    if parsed_args.cover:
+        argv.extend(['--with-coverage', '--cover-package=%s' % package_name,
+                     '--cover-erase', '--cover-inclusive',
+                     '--cover-branches'])
+
+    if parsed_args.cover_tests:
+        argv.append('--cover-tests')
+
+    run(argv=argv)
