dc7b56adc511f72c91d87e02a2b8e982ae23c834
Phillip Cloud <cpcloud@gmail.com>
2013-03-30 16:27:38 -0400

    update testing script

--- test.py
+++ test.py
@@ -11,9 +11,10 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--cover-tests', action='store_true',
                         default=False)
     parser.add_argument('-c', '--cover', action='store_true', default=False)
+    parser.add_argument('-v', '--verbose', action='store_true', default=False)
     parsed_args = parser.parse_args()
 
-    package_name = os.path.basename(os.path.dirname(os.curdir))
+    package_name = os.path.basename(os.path.abspath(os.curdir))
 
     argv = ['nosetests', '-w', package_name]
 
@@ -22,8 +23,7 @@ if __name__ == '__main__':
 
     if parsed_args.cover:
         argv.extend(['--with-coverage', '--cover-package=%s' % package_name,
-                     '--cover-erase', '--cover-inclusive',
-                     '--cover-branches'])
+                     '--cover-erase', '--cover-inclusive', '--cover-branches'])
 
     if parsed_args.cover_tests:
         argv.append('--cover-tests')
