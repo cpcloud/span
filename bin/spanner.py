1701c3aef73ff5c55b1b52ffb25dc5d585309288
Phillip Cloud <cpcloud@gmail.com>
2013-05-17 09:41:27 -0400

    conversion ready for testing

--- bin/spanner.py
+++ bin/spanner.py
@@ -5,15 +5,16 @@ import os
 import argparse
 import numbers
 
+import numpy as np
 import pandas as pd
-import IPython as ipy
-from clint.textui import puts
-from clint.textui.colored import red
 
-from span import TdtTank, NeuroNexusMap, spike_xcorr
+
+CHAR_BIT = 8
 
 
 def error(msg):
+    from clint.textui import puts
+    from clint.textui.colored import red
     errmsg = os.path.basename(__file__)
     errmsg += ': error: {0}'.format(msg)
     puts(red(errmsg))
@@ -56,6 +57,7 @@ class SpanCommand(object):
 
 class Analyzer(SpanCommand):
     def _load_data(self, return_tank=False):
+        from span import TdtTank, NeuroNexusMap
         tank = TdtTank(self.filename, NeuroNexusMap)
         spikes = tank.spikes
 
@@ -65,49 +67,128 @@ class Analyzer(SpanCommand):
             return spikes
 
     def _run(self, args):
-        pass
+        raise NotImplementedError()
+
+
+def _compute_xcorr(spikes, args):
+    import span
+    from span import spike_xcorr
+    detrend = 'detrend_' + args.detrend
+    thr = spikes.threshold(args.threshold)
+    thr.clear_refrac(ms=args.refractory_period, inplace=True)
+    binned = thr.bin(args.bin_size, how=args.bin_method)
+    xc = spike_xcorr(binned, max_lags=args.max_lags,
+                     scale_type=args.scale_type,
+                     detrend=getattr(span, detrend), nan_auto=args.nan_auto)
+    return xc
+
+
+def _build_plot_filename(tank):
+    raise NotImplementedError()
 
 
 class CorrelationAnalyzer(Analyzer):
-    def _compute_xcorr(self, args):
-        threshold = args.threshold
-        bin_size = args.bin_size
-        scale_type = args.scale_type
-        detrend = args.detrend
-        max_lags = args.max_lags
-        nan_auto = args.nan_auto
-        ms = args.refractory_period
-        bin_method = args.bin_method
-
-        spikes = self._load_data()
-        thr = spikes.threshold(threshold)
-        thr.clear_refrac(ms=ms, inplace=True)
-        binned = thr.bin(bin_size, how=bin_method, reduced=False)
-        xc = spike_xcorr(binned, max_lags=max_lags, scale_type=scale_type,
-                         detrend=detrend, nan_auto=nan_auto)
-        return xc
-
     def _run(self, args):
-        display = args.display
-        xc = self._compute_xcorr(args)
+        tank, spikes = self._load_data(return_tank=True)
+        xc = self._compute_xcorr(spikes, args)
 
-        if display:
-            self._display_xcorr(xc)
+        if args.display:
+            plot_filename = _build_plot_filename(tank)
+            self._display_xcorr(xc, plot_filename)
 
-    def _display_xcorr(self, xc):
+    def _display_xcorr(self, xc, plot_filename):
         pass
 
 
 class IPythonAnalyzer(Analyzer):
     """Drop into an IPython shell given a filename or database id number"""
     def _run(self, args):
-        tank = self._load_data()
+        import IPython as ipy
+        tank, spikes = self._load_data(return_tank=True)
         ipy.embed_kernel(local_ns={'tank': tank, 'raw': tank.spikes})
+
+
+class BaseConverter(object):
+    store_index = False
+    store_fs = False
+
+    def __init__(self, base_type, precision, date):
+        self.base_type, self.precision = base_type, precision
+        self.dtype = np.dtype(self.__base_type + str(self._precision))
+        self.date = date
+
+    def split_data(self, raw):
+        shank = raw.columns.get_level_values('shank').values
+        channels = raw.columns.get_level_values('channels').values
+        index = raw.index.values
+        values = raw.values
+        fs = raw.fs
+        date = self.date
+        elapsed = (raw.index.freq.n +
+                   np.zeros(raw.nsamples)).cumsum().astype('m8[ns]')
+        return locals()
+
+    def convert(self, raw, outfile):
+        if not self.store_index:
+            raw.sortlevel('channel', axis=1, inplace=True)
+
+        if not self.store_fs:
+            base, ext = os.path.splitext(outfile)
+            outfile = '{base}{fs}{extsep}{ext}'.format(base=base, fs=raw.fs,
+                                                       extsep=os.extsep,
+                                                       ext=ext)
+        self._convert(raw, outfile)
+
+
+class NeuroscopeConverter(BaseConverter):
+    def _convert(self, raw, outfile):
+        from bottleneck import nanmax
+        max_prec = float(2 ** (self.precision * CHAR_BIT - 1) - 1)
+        const = max_prec / nanmax(np.abs(raw))
+        xc = raw.values * const
+        xc.astype(self.dtype).tofile(outfile)
+
+
+class H5Converter(BaseConverter):
+    store_index = True
+    store_fs = True
+
+    def _convert(self, raw, outfile):
+        raw.to_hdf(outfile, 'raw')
+
+
+class NumPyConverter(BaseConverter):
+    store_index = True
+    store_fs = True
+
+    def _convert(self, raw, outfile):
+        split = self.split_data(raw)
+        values = split['values']
+
+        if self.dtype != values.dtype:
+            split['values'] = values.astype(self.dtype)
+
+        np.savez(outfile, **split)
+
+
+class MATLABConverter(BaseConverter):
+    store_index = True
+    store_fs = True
+
+    def _convert(self, raw, outfile):
+        from scipy.io import savemat
+        savemat(outfile, self.split_data(raw))
+
+
+_converters = {'neuroscope': NeuroscopeConverter, 'matlab': MATLABConverter,
+               'h5': H5Converter, 'numpy': NumPyConverter}
 
 
 class Converter(SpanCommand):
     def _run(self, args):
-        pass
+        spikes = self._load_data()
+        converter = _converters[args.format](args.base_dtype, args.precision)
+        converter.convert(spikes, args.outfile)
 
 
 # get the filename/id, convert to neuroscope with int16 precision, zip into
@@ -141,22 +222,26 @@ class DbDeleter(Db):
         pass
 
 
-def build_analyze_parser(subparsers, parent):
+def build_analyze_parser(subparsers):
     def build_correlation_parser(subparsers):
         parser = subparsers.add_parser('correlation', help='perform cross '
                                        'correlation analysis on a recording')
         add_filename_and_id_to_parser(parser)
         parser.add_argument('-d', '--display', action='store_true')
         parser.add_argument('-t', '--threshold', type=float)
-        parser.add_argument('-r', '--refractory-period', type=int)
+        parser.add_argument('-r', '--refractory-period', type=int, default=2)
         parser.add_argument('-b', '--bin-size', type=int)
-        parser.add_argument('-p', '--bin-method')
+        parser.add_argument('-p', '--bin-method', default='sum')
         parser.add_argument('-s', '--scale-type', choices=('normalize', 'none',
                                                            'biased',
-                                                           'unbiased'))
-        parser.add_argument('-m', '--detrend')
-        parser.add_argument('-l', '--max-lags', type=int)
-        parser.add_argument('-n', '--nan-auto', action='store_true')
+                                                           'unbiased'),
+                            default='normalize')
+        parser.add_argument('-m', '--detrend', choices=('mean', 'linear',
+                                                        'none'),
+                            default='mean')
+        parser.add_argument('-l', '--max-lags', type=int, default=1)
+        parser.add_argument('-n', '--nan-auto', action='store_true',
+                            default=True)
         parser.set_defaults(run=CorrelationAnalyzer().run)
 
     def build_ipython_parser(subparsers):
@@ -172,7 +257,7 @@ def build_analyze_parser(subparsers, parent):
     build_ipython_parser(subparsers)
 
 
-def build_convert_parser(subparsers, parent):
+def build_convert_parser(subparsers):
     parser = subparsers.add_parser('convert', help='convert a TDT tank file '
                                    'into a different format')
     add_filename_and_id_to_parser(parser)
@@ -190,30 +275,43 @@ def build_convert_parser(subparsers, parent):
     parser.set_defaults(run=Converter().run)
 
 
-def build_view_parser(subparsers, parent):
+def build_view_parser(subparsers):
     parser = subparsers.add_parser('view', help='display the raw traces of a '
-                                   'TDT tank file in Neuroscope',
-                                   parents=[parent])
+                                   'TDT tank file in Neuroscope')
+    add_filename_and_id_to_parser(parser)
     parser.set_defaults(run=Viewer().run)
 
 
-def build_db_parser(subparsers, parent):
+def build_db_parser(subparsers):
+    def _add_args_to_parser(parser):
+        parser.add_argument('-a', '--age', type=int, help='the age of the '
+                            'animal')
+        parser.add_argument('-c', '--condition', help='the experimental '
+                            'condition, if any')
+        parser.add_argument('-w', '--weight', type=float, help='the weight '
+                            'of the animal')
+        parser.add_argument('-b', '--bad', action='store_true', help='Mark '
+                            'a recording as "good"')
+
     def build_db_create_parser(subparsers):
         parser = subparsers.add_parser('create', help='put a new recording in '
                                        'the database')
         add_filename_and_id_to_parser(parser)
+        _add_args_to_parser(parser)
         parser.set_defaults(run=DbCreator().run)
 
     def build_db_read_parser(subparsers):
         parser = subparsers.add_parser('read', help='query the properties of a'
                                        ' recording')
         add_filename_and_id_to_parser(parser)
+        _add_args_to_parser(parser)
         parser.set_defaults(run=DbReader().run)
 
     def build_db_update_parser(subparsers):
         parser = subparsers.add_parser('update', help='update the properties '
                                        'of an existing recording')
         add_filename_and_id_to_parser(parser)
+        _add_args_to_parser(parser)
         parser.set_defaults(run=DbUpdater().run)
 
     def build_db_delete_parser(subparsers):
@@ -221,18 +319,11 @@ def build_db_parser(subparsers, parent):
                                        'recordings matching certain '
                                        'conditions')
         add_filename_and_id_to_parser(parser)
+        _add_args_to_parser(parser)
         parser.set_defaults(run=DbDeleter().run)
 
     parent_parser = subparsers.add_parser('db', help='Operate on the database '
                                           'of recordings', add_help=False)
-    parent_parser.add_argument('-a', '--age', type=int, help='the age of the '
-                               'animal')
-    parent_parser.add_argument('-c', '--condition', help='the experimental '
-                               'condition, if any')
-    parent_parser.add_argument('-w', '--weight', type=float, help='the weight '
-                               'of the animal')
-    parent_parser.add_argument('-b', '--bad', action='store_true', help='Mark '
-                               'a recording as "good"')
     subparsers = parent_parser.add_subparsers(description='use the following '
                                               'subcomands to perform specific '
                                               'operations on the database of '
@@ -250,15 +341,14 @@ def add_filename_and_id_to_parser(parser):
 
 
 def main():
-    parent_parser = argparse.ArgumentParser(description='analyze TDT tank '
-                                            'files', add_help=False)
-    subparsers = parent_parser.add_subparsers(help='subcommands for analying '
-                                              'TDT tankfiles')
-    build_analyze_parser(subparsers, parent_parser)
-    build_convert_parser(subparsers, parent_parser)
-    build_view_parser(subparsers, parent_parser)
-    build_db_parser(subparsers, parent_parser)
-    args = parent_parser.parse_args()
+    parser = argparse.ArgumentParser(description='analyze TDT tank files')
+    subparsers = parser.add_subparsers(help='subcommands for analying TDT '
+                                       'tank files')
+    build_analyze_parser(subparsers)
+    build_convert_parser(subparsers)
+    build_view_parser(subparsers)
+    build_db_parser(subparsers)
+    args = parser.parse_args()
     return args.run(args)
 
 
