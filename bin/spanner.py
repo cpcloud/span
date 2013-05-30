e9f81771b8d58baa663f030c1b1a5d41d2909681
Phillip Cloud <cpcloud@gmail.com>
2013-05-30 17:09:09 -0400

    hack together some stuff for the lab

--- bin/spanner.py
+++ bin/spanner.py
@@ -4,6 +4,9 @@ import sys
 import re
 import argparse
 
+import numpy as np
+import pandas as pd
+
 from dateutil.parser import parse as _parse_date
 
 from span.spanner.db import Db, DbCreator, DbReader, DbUpdater, DbDeleter
@@ -12,6 +15,9 @@ from span.spanner.converters import Converter
 from span.spanner.viewer import Viewer
 from span.spanner.utils import _init_db
 from span.spanner.defaults import SPAN_DB
+
+import span
+from span import TdtTank, NeuroNexusMap, ElectrodeMap
 
 
 def _colon_to_slice(c):
@@ -26,9 +32,72 @@ def _parse_artifact_ranges(s):
     return [_colon_to_slice(spl) for spl in split]
 
 
+def _get_xcorr(sp, threshold, sd, binsize, how, firing_rate_threshold, maxlags,
+               which_lag, refractory_period, nan_auto, detrend, scale_type):
+    thr = sp.threshold(threshold * sd)
+    thr.clear_refrac(refractory_period, inplace=True)
+
+    binned = thr.resample(binsize, how=how)
+    binned.ix[:, binned.mean() < firing_rate_threshold] = np.nan
+
+    xc = thr.xcorr(binned, maxlags=maxlags, detrend=getattr(span, 'detrend_' +
+                                                            detrend),
+                   scale_type=scale_type, nan_auto=nan_auto).ix[which_lag]
+    xc.name = threshold
+    return xc
+
+
+def _get_xcorr_many_threshes(sp, threshes, sd, binsize, how,
+                             firing_rate_threshold, maxlags, which_lag,
+                             refractory_period, nan_auto, detrend, scale_type,
+                             distance_map):
+    xcs = pd.concat([_get_xcorr(sp, thresh, sd, binsize, how,
+                                firing_rate_threshold, maxlags, which_lag,
+                                refractory_period, nan_auto, detrend,
+                                scale_type) for thresh in threshes], axis=1)
+    xcs['distance'] = distance_map
+    xcs.sort('distance', inplace=True)
+    xcs = xcs.dropna(axis=(0, 1), how='all')
+    xcs.drop_duplicates(inplace=True)
+    return xcs
+
+
+def _get_cutoff(df, thresh, dist_key='distance'):
+    f = lambda x: pd.isnull(x).sum()
+    cols = df.columns
+    null_count_diff = df[cols[cols != dist_key]].apply(f).diff()
+    cutoff = np.nan
+    for ind, el in null_count_diff.iteritems():
+        if np.floor(np.log10(el)) >= thresh:
+            cutoff = ind
+            break
+
+    if pd.isnull(cutoff):
+        return null_count_diff[null_count_diff > 0].index[0]
+    return cutoff
+
+
+def _trim_concatted(xcs_df, ord_mag_thresh=2, dist_key='distance'):
+    cutoff = _get_cutoff(xcs_df, ord_mag_thresh)
+    df = xcs_df.ix[:, xcs_df.columns >= cutoff].sort_index(axis=1)
+    cols = df.columns
+    subset = cols[cols != dist_key]
+    return df.dropna(axis=0, how='all', subset=subset).dropna(axis=1,
+                                                              how='all')
+
+
+def _concat(xcs, threshes):
+    xc_all = xcs.copy()
+    xc_all.distance /= xc_all.distance.max()
+    dist_key = 'distance'
+    subset = xc_all.columns[xc_all.columns != dist_key]
+    xc_df = xc_all.dropna(axis=0, how='all', subset=subset)
+    return xc_df
+
+
 def compute_xcorr(args):
     filename = args.filename
-    
+
     # make a tank
     em = ElectrodeMap(NeuroNexusMap.values, args.within_shank,
                       args.between_shank)
@@ -37,10 +106,72 @@ def compute_xcorr(args):
     # get the raw data
     spikes = tank.spik
 
+    if args.remove_first_pc:
+        span.remove_first_pc(spikes)
+
     # get the threshes
-    threshes = linspace(args.min_threshold, args.max_threshold,
-                        args.num_thresholds)
+    threshes = np.linspace(args.min_threshold, args.max_threshold,
+                           args.num_thresholds)
+
     # compute a few different thresholds
+    sd = spikes.std()
+
+    # compute the cross correlation at each threshold
+    xcs = _get_xcorr_many_threshes(spikes, threshes, sd, args.bin_size,
+                                   args.bin_method, args.firing_rate_threshold,
+                                   args.max_lags, args.which_lag,
+                                   args.refractory_period, not args.keep_auto,
+                                   args.detrend, args.scale_type,
+                                   em.distance_map())
+    # concat all xcorrs
+    xcs_df = _concat(xcs, threshes)
+
+    # remove the data that is useless
+    trimmed = _trim_concatted(xcs_df)
+
+    return trimmed, tank.age, tank.site
+
+
+def show_xcorr(args):
+    from mpltools import style
+    from mpltools.layout import figaspect
+    style.use('ggplot')
+    from mpl_toolkits.axes_grid1 import ImageGrid
+    from pylab import figure, colorbar, rcParams
+    from bottleneck import nanmax, nanmin
+    trimmed, age, site = compute_xcorr(args)
+
+    vmax = nanmax(trimmed.values)
+    vmin = nanmin(trimmed.values)
+
+    fig = figure(figsize=figaspect(1))
+    grid = ImageGrid(fig, 111, nrows_ncols=(1, 1), direction='row',
+                     axes_pad=0.05, add_all=True, label_mode='1',
+                     share_all=False, cbar_location='right',
+                     cbar_mode='single', cbar_size='10%', cbar_pad=0.05)
+    ax = grid[0]
+    trimmed.sortlevel('shank i', inplace=True)
+    trimmed.sortlevel('shank j', inplace=True)
+    trimmed.set_index(['distance'], append=True, inplace=True, drop=True)
+    im = ax.imshow(trimmed.values, interpolation='none', aspect='auto',
+                   vmax=vmax, vmin=vmin)
+    m, n = trimmed.shape
+    ax.set_xticks(np.arange(n))
+    ax.set_xticklabels(map('{0:.1f}'.format,
+                           trimmed.columns.values.astype(float)))
+    ax.tick_params(labelsize=3, left=False, top=False, right=False,
+                   bottom=False)
+    ax.cax.colorbar(im)
+    ax.cax.tick_params(labelsize=5, right=False)
+    ax.set_yticks(np.arange(m))
+    ax.set_xlabel('Threshold (multiples of standard deviation)', fontsize=6)
+    rcParams['text.usetex'] = True
+    f = lambda x: r'\textbf{{0}}, {1}, \textbf{{2}}, {3}, {4:.1f}'.format(*x)
+    ax.set_yticklabels(map(f, trimmed.index))
+    ax.set_ylabel('shank i, channel i, shank j, channel j % of max distance',
+                  fontsize=6)
+    fig.tight_layout()
+    fig.savefig('figname1.pdf', bbox_inches='tight')
 
 
 def build_analyze_parser(subparsers):
@@ -49,6 +180,7 @@ def build_analyze_parser(subparsers):
                                        'correlation analysis on a recording',
                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument('-f', '--filename', help='filename')
+        parser.add_argument('-i', '--id', help='id')
         cleaning = parser.add_argument_group('cleaning')
         display = parser.add_argument_group('display')
         thresholding = parser.add_argument_group('thresholding')
@@ -59,41 +191,48 @@ def build_analyze_parser(subparsers):
         display.add_argument('-d', '--display', action='store_true',
                              help='display the resulting cross correlation analysis')
         thresholding.add_argument(
-            '-T', '--max-threshold', type=float, required=True,
+            '-T', '--max-threshold', type=float, default=4.0,
             help='maximum threshold in multiples of the standard deviation of the voltage data')
         thresholding.add_argument(
-            '-t', '--min-threshold', type=float, default=1.0,
+            '-t', '--min-threshold', type=float, default=3.0,
             help='minimum threshold in multiples of the standard deviation of the voltage data')
         thresholding.add_argument('-n', '--num-thresholds', type=int, default=50)
         thresholding.add_argument(
             '-r', '--refractory-period', type=int, default=2, help='refractory period in milliseconds')
         binning.add_argument(
-            '-b', '--bin-size', type=int, default='1S', help='bin size in some time unit')
+            '-b', '--bin-size', default='S', help='bin size in some time unit')
         binning.add_argument(
             '-p', '--bin-method', default='sum', help='function to use for binning spikes')
+        binning.add_argument(
+            '-R', '--firing-rate-threshold', type=float, default=1.0)
+        xcorr.add_argument('-w', '--within-shank', type=float, default=50.0)
+        xcorr.add_argument('-W', '--between-shank', type=float, default=125.0)
         xcorr.add_argument(
             '-s', '--scale-type', choices=('normalize', 'none', 'biased',
-                                           'unbiased'), default='normalize', help='type of scaling to use on the raw cross correlation')
+                                           'unbiased'), default='normalize',
+            help='type of scaling to use on the raw cross correlation')
         xcorr.add_argument(
             '-m', '--detrend', choices=('mean', 'linear', 'none'),
             default='mean', help='function to use to detrend the raw cross correlation')
         xcorr.add_argument('-l', '--max-lags', type=int, default=1,
                            help='maximum number of lags of the cross correlation to return')
+        xcorr.add_argument('-L', '--which-lag', type=int, default=0)
         xcorr.add_argument(
             '-k', '--keep-auto', action='store_true', help='keep the autocorrelation values')
-        parser.set_defaults(run=CorrelationAnalyzer().run)
+        parser.set_defaults(run=show_xcorr)
 
-
+    parser = subparsers.add_parser('analyze', help='perform an analysis on a '
+                                   'TDT tank file')
+    subparsers = parser.add_subparsers()
+    build_correlation_parser(subparsers)
 
 
 class DateParseAction(argparse.Action):
-
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, _parse_date(values))
 
 
 class ArtifactRangesAction(argparse.Action):
-
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, _parse_artifact_ranges(values))
 
@@ -218,13 +357,16 @@ def main():
     subparsers = parser.add_subparsers(help='Subcommands for analying TDT '
                                        'tank files')
     build_analyze_parser(subparsers)
-    build_convert_parser(subparsers)
-    build_view_parser(subparsers)
-    build_db_parser(subparsers)
+    #build_convert_parser(subparsers)
+    #build_view_parser(subparsers)
+    #build_db_parser(subparsers)
     args = parser.parse_args()
     return args.run(args)
 
 
 if __name__ == '__main__':
-    _init_db(SPAN_DB, Db)
+    try:
+        _init_db(SPAN_DB, Db)
+    except:
+        pass
     sys.exit(main())
