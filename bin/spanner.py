0f2c4b1d2815f02e2508c36cc04b1ee4c9dc85cb
Phillip Cloud <cpcloud@gmail.com>
2013-06-05 14:34:38 -0400

    CLN: move correlation analysis stuff to approriate file

--- bin/spanner.py
+++ bin/spanner.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import re
 import argparse
-import os
-import warnings
-
-import numpy as np
-import pandas as pd
 
 from dateutil.parser import parse as _parse_date
 
@@ -17,166 +11,6 @@ from span.spanner.converters import Converter
 from span.spanner.viewer import Viewer
 from span.spanner.utils import _init_db
 from span.spanner.defaults import SPAN_DB
-
-import span
-from span import TdtTank, NeuroNexusMap, ElectrodeMap
-
-
-def _colon_to_slice(c):
-    splitter = re.compile(r'\s*:\s*')
-    start, stop = splitter.split(c)
-    return slice(int(start), int(stop))
-
-
-def _parse_artifact_ranges(s):
-    splitter = re.compile(r'\s*,\s*')
-    split = splitter.split(s)
-    return [_colon_to_slice(spl) for spl in split]
-
-
-def _get_xcorr(sp, threshold, sd, binsize, how, firing_rate_threshold, maxlags,
-               which_lag, refractory_period, nan_auto, detrend, scale_type):
-    thr = sp.threshold(threshold * sd)
-    thr.clear_refrac(refractory_period, inplace=True)
-
-    binned = thr.resample(binsize, how=how)
-    binned.ix[:, binned.mean() < firing_rate_threshold] = np.nan
-
-    xc = thr.xcorr(binned, maxlags=maxlags, detrend=getattr(span, 'detrend_' +
-                                                            detrend),
-                   scale_type=scale_type, nan_auto=nan_auto).ix[which_lag]
-    xc.name = threshold
-    return xc
-
-
-def _get_xcorr_many_threshes(sp, threshes, sd, binsize, how,
-                             firing_rate_threshold, maxlags, which_lag,
-                             refractory_period, nan_auto, detrend, scale_type,
-                             distance_map):
-    xcs = pd.concat([_get_xcorr(sp, thresh, sd, binsize, how,
-                                firing_rate_threshold, maxlags, which_lag,
-                                refractory_period, nan_auto, detrend,
-                                scale_type) for thresh in threshes], axis=1)
-    xcs['distance'] = distance_map
-    xcs.sort('distance', inplace=True)
-    xcs = xcs.dropna(axis=(0, 1), how='all')
-    xcs.drop_duplicates(inplace=True)
-    return xcs
-
-
-def _get_cutoff(df, thresh, dist_key='distance'):
-    f = lambda x: pd.isnull(x).sum()
-    cols = df.columns
-    null_count_diff = df[cols[cols != dist_key]].apply(f).diff()
-    cutoff = np.nan
-    for ind, el in null_count_diff.iteritems():
-        if np.floor(np.log10(el)) >= thresh:
-            cutoff = ind
-            break
-
-    if pd.isnull(cutoff):
-        return null_count_diff[null_count_diff > 0].index[0]
-    return cutoff
-
-
-def _trim_concatted(xcs_df, ord_mag_thresh=2, dist_key='distance'):
-    cutoff = _get_cutoff(xcs_df, ord_mag_thresh)
-    df = xcs_df.ix[:, xcs_df.columns >= cutoff].sort_index(axis=1)
-    cols = df.columns
-    subset = cols[cols != dist_key]
-    return df.dropna(axis=0, how='all', subset=subset).dropna(axis=1,
-                                                              how='all')
-
-
-def _concat(xcs, threshes):
-    xc_all = xcs.copy()
-    xc_all.distance /= xc_all.distance.max()
-    dist_key = 'distance'
-    subset = xc_all.columns[xc_all.columns != dist_key]
-    xc_df = xc_all.dropna(axis=0, how='all', subset=subset)
-    return xc_df
-
-
-def compute_xcorr(args):
-    filename = args.filename
-
-    # make a tank
-    em = ElectrodeMap(NeuroNexusMap.values, args.within_shank,
-                      args.between_shank)
-    tank = TdtTank(filename, em)
-
-    # get the raw data
-    spikes = tank.spik
-
-    if args.remove_first_pc:
-        span.remove_first_pc(spikes)
-
-    # get the threshes
-    threshes = np.linspace(args.min_threshold, args.max_threshold,
-                           args.num_thresholds)
-
-    # compute a few different thresholds
-    sd = spikes.std()
-
-    # compute the cross correlation at each threshold
-    xcs = _get_xcorr_many_threshes(spikes, threshes, sd, args.bin_size,
-                                   args.bin_method, args.firing_rate_threshold,
-                                   args.max_lags, args.which_lag,
-                                   args.refractory_period, not args.keep_auto,
-                                   args.detrend, args.scale_type,
-                                   em.distance_map())
-    # concat all xcorrs
-    xcs_df = _concat(xcs, threshes)
-
-    # remove the data that is useless
-    trimmed = _trim_concatted(xcs_df)
-
-    return trimmed, tank.age, tank.site
-
-
-def show_xcorr(args):
-    import matplotlib as mpl
-    mpl.use('pdf')
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.axes_grid1 import ImageGrid
-    from bottleneck import nanmax, nanmin
-    trimmed, age, site = compute_xcorr(args)
-
-    vmax = nanmax(trimmed.values)
-    vmin = nanmin(trimmed.values)
-
-    fig = plt.figure(figsize=(16, 9))
-    grid = ImageGrid(fig, 111, nrows_ncols=(1, 1), direction='row',
-                     axes_pad=0.05, add_all=True, label_mode='1',
-                     share_all=False, cbar_location='right',
-                     cbar_mode='single', cbar_size='10%', cbar_pad=0.05)
-    ax = grid[0]
-    trimmed.sortlevel('shank i', inplace=True)
-    trimmed.sortlevel('shank j', inplace=True)
-    trimmed.set_index(['distance'], append=True, inplace=True, drop=True)
-    im = ax.imshow(trimmed.values, interpolation='none', aspect='auto',
-                   vmax=vmax, vmin=vmin)
-    m, n = trimmed.shape
-
-    ax.set_xticks(np.arange(n))
-    ax.set_xticklabels(map('{0:.1f}'.format,
-                           trimmed.columns.values.astype(float)))
-    ax.tick_params(labelsize=3, left=False, top=False, right=False,
-                   bottom=False)
-    ax.cax.colorbar(im)
-    ax.cax.tick_params(labelsize=5, right=False)
-    ax.set_yticks(np.arange(m))
-    ax.set_xlabel('Threshold (multiples of standard deviation)', fontsize=6)
-    mpl.rcParams['text.usetex'] = True
-    f = lambda x: r'\textbf{{0}}, {1}, \textbf{{2}}, {3}, {4:.1f}'.format(*x)
-    ax.set_yticklabels(map(f, trimmed.index))
-    ax.set_ylabel('shank i, channel i, shank j, channel j % of max distance',
-                  fontsize=6)
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore', UserWarning)
-        fig.tight_layout()
-        fig.savefig('{0}{1}pdf'.format(os.path.splitext(args.filename)[0],
-                                       os.extsep), bbox_inches='tight')
 
 
 def build_analyze_parser(subparsers):
@@ -224,7 +58,7 @@ def build_analyze_parser(subparsers):
         xcorr.add_argument('-L', '--which-lag', type=int, default=0)
         xcorr.add_argument(
             '-k', '--keep-auto', action='store_true', help='keep the autocorrelation values')
-        parser.set_defaults(run=show_xcorr)
+        parser.set_defaults(run=CorrelationAnalyzer().run)
 
     parser = subparsers.add_parser('analyze', help='perform an analysis on a '
                                    'TDT tank file')
@@ -246,17 +80,16 @@ def build_convert_parser(subparsers):
     parser = subparsers.add_parser('convert', help='convert a TDT tank file '
                                    'into a different format')
     add_filename_and_id_to_parser(parser)
-    parser.add_argument('-t', '--type',
-                        help='the type of conversion you want to '
-                        'perform', choices=('neuroscope', 'matlab', 'numpy',
-                                            'h5'), required=True)
-    parser.add_argument('-d', '--base-type',
+    parser.add_argument('-t', '--format', help='the type of conversion '
+                        'you want to perform', choices=('neuroscope', 'igor',
+                        'matlab', 'numpy', 'h5'), required=True)
+    parser.add_argument('-T', '--numeric-type',
                         help='the base numeric type to convert to',
-                        default='float', choices=('float', 'int', 'uint', 'f',
-                                                  'i', 'ui'), required=True)
+                        default='int', choices=('float', 'int', 'uint', 'f',
+                                                  'i', 'ui'))
     parser.add_argument('-p', '--precision', help='the number of bits '
-                        'to use for conversion', type=int, default=64,
-                        choices=(8, 16, 32, 64), required=True)
+                        'to use for conversion', type=int, default=16,
+                        choices=(8, 16, 32, 64))
     parser.set_defaults(run=Converter().run)
 
 
